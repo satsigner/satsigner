c0ee53db8b1b0377ceab1bf270c2964361657f51
tmakerman <116613158+tmakerman@users.noreply.github.com>
2022-12-23 16:37:01 -0500

    Adds listing of 12 numbered word text inputs.

--- src/components/accounts/ImportSeedScreen.tsx
+++ src/components/accounts/ImportSeedScreen.tsx
@@ -2,10 +2,11 @@ import React from 'react';
 import {
   View,
   Text,
+  TextInput,
   StyleSheet,
 } from 'react-native';
 
-import { Typography, Layout } from '../../styles';
+import { Typography, Layout, Colors } from '../../styles';
 
 import Account from '../../models/Account';
 
@@ -27,17 +28,34 @@ export default class ImportSeedScreen extends React.PureComponent<Props, State> 
   }
 
   render() {
+    const words = [];
+    for (let i = 0; i < 12; i++) {
+      words.push(<Word num={i+1}></Word>);
+    }
+    
     return (
       <View style={styles.container}>
         <View>
           <Text style={styles.label}>
             Mnemonic Seed Words (BIP39)
           </Text>
+          <View style={styles.words}>
+            {words}
+          </View>
         </View>
       </View>
     );
   }
 
+}
+
+function Word(props: any) {
+  return (
+    <View style={styles.word}>
+      <TextInput style={styles.wordText}></TextInput>
+      <Text style={styles.wordNumLabel}>{props.num}</Text>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({  
@@ -51,4 +69,31 @@ const styles = StyleSheet.create({
     alignSelf: 'center',
     marginBottom: 7
   },
+  words: {
+    flexDirection: 'column',
+    flexWrap: 'wrap',
+    justifyContent: 'space-between',
+    alignContent: 'space-between',
+    height: 198
+  },
+  word: {
+    height: 44,
+    width: '32%',
+    justifyContent: 'flex-start',
+    alignContent: 'center',
+  },
+  wordNumLabel: {
+    position: 'absolute',
+    top: 2,
+    left: 5,
+    ...Typography.textNormal.x5,
+  },
+  wordText: {
+    ...Typography.textHighlight.x9,
+    backgroundColor: Colors.inputBackground,
+    fontWeight: '300',
+    textAlign: 'center',
+    borderRadius: 3,
+    letterSpacing: 0.6
+  }
 });
