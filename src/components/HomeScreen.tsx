765c46142607b22bb175de274be33dddd484e279
tmakerman <116613158+tmakerman@users.noreply.github.com>
2024-03-16 00:46:10 -0400

    Styling for word confirmation checkboxes.  Includes react-native-bouncy-checkbox-group dependency.

--- src/components/HomeScreen.tsx
+++ src/components/HomeScreen.tsx
@@ -4,11 +4,15 @@ import {
   Text,
   StyleSheet,
 } from 'react-native';
-import { NavigationProp } from '@react-navigation/native';
 
-import { Typography, Layout } from '../styles';
+import { NavigationProp } from '@react-navigation/native';
+import BouncyCheckboxGroup, { ICheckboxButton } from 'react-native-bouncy-checkbox-group';
+
+import { Typography, Layout, Colors } from '../styles';
 
 import Button from './shared/Button';
+
+const CheckIcon = require('../assets/images/check.png');
 
 interface Props {
   navigation: NavigationProp<any>
@@ -35,6 +39,36 @@ export default class HomeScreen extends React.PureComponent<Props, State> {
         </View>
         <View style={styles.actions}>
           <Button title='Account List' onPress={() => this.props.navigation.navigate('AccountList')}></Button>
+        </View>
+        <View>
+        <BouncyCheckboxGroup
+          data={[
+            {
+              id: 0,
+              iconStyle: {
+                borderRadius: 4
+              },
+              iconImageStyle: {
+                tintColor: Colors.grey191,
+                width: 17,
+                height: 17                
+              },
+              checkIconImageSource: CheckIcon,
+              size: 32,
+              innerIconStyle: {
+                borderRadius: 4,
+                borderWidth: 2,
+                borderColor: 'rgba(255, 255, 255, 0.68)'
+              },
+              unfillColor: 'rgba(255, 255, 255, 0.17)',
+              fillColor: 'rgba(255, 255, 255, 0.22)',
+            }
+          ]}
+          style={{ flexDirection: "column" }}
+          onChange={(selectedItem: ICheckboxButton) => {
+            console.log("SelectedItem: ", JSON.stringify(selectedItem));
+          }}
+        />
         </View>
       </View>
     );
