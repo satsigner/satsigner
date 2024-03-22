229ae4d83ae986c3b2fb1508736c8e47df284a48
tmakerman <116613158+tmakerman@users.noreply.github.com>
2024-03-21 22:53:21 -0400

    Refactor AccountAddedModal to use new ModalDialog component

--- src/components/accounts/AccountAddedModal.tsx
+++ src/components/accounts/AccountAddedModal.tsx
@@ -8,8 +8,8 @@ import { EllipsisAnimation } from '../shared/EllipsisAnimation';
 
 import { AccountsContext } from './AccountsContext';
 
-import Button from '../shared/Button';
 import { AppText } from '../shared/AppText';
+import ModalDialog from '../shared/ModalDialog';
 
 import LinearGradient from 'react-native-linear-gradient';
 
@@ -60,119 +60,92 @@ export default class AccountAddedModal extends React.PureComponent<Props, State>
     return (
       <AccountsContext.Consumer>
         {({ currentAccount }) => (
-          <View style={styles.dimmedOverlay}>
-            <LinearGradient
-              style={styles.dialogBackground}
-              colors={[Colors.grey21, Colors.grey47]}
-              start={{ x: 1, y: 1 }}
-              end={{ x: 0, y: 0 }}
-            >
-              <View style={{...styles.columnSection, height: 137 }}>
-                <AppText style={styles.heading}>{currentAccount.name}</AppText>
-                <AppText style={styles.subheading}>Parent account has been added</AppText>
-              </View>
+          <ModalDialog
+            buttonText='Close'
+            onClose={this.props.onClose}
+          >
+            <View style={{...styles.columnSection, height: 137 }}>
+              <AppText style={styles.heading}>{currentAccount.name}</AppText>
+              <AppText style={styles.subheading}>Parent account has been added</AppText>
+            </View>
 
-              <Separator />
-              
-              <View style={{...styles.columnSection, height: 112}}>
-                <View style={{...styles.rowSection, alignItems: 'flex-start' }}>
-                    <View style={styles.columnSection}>
-                      <AppText style={styles.label}>Script</AppText>
-                      <AppText style={{...styles.valueSmall, marginTop: 2 }}>{this.getScriptLongName(currentAccount.scriptVersion as ScriptVersion)}</AppText>
-                      <AppText style={styles.valueSmall}>({this.getScriptShortName(currentAccount.scriptVersion as ScriptVersion)})</AppText>
-                    </View>
-                    <View style={styles.columnSection}>
-                      <AppText style={styles.label}>Fingerprint</AppText>
-                      <AppText style={{...styles.valueSmall, marginTop: 2 }}>{currentAccount?.fingerprint}</AppText>
-                    </View>
+            <Separator />
+            
+            <View style={{...styles.columnSection, height: 112}}>
+              <View style={{...styles.rowSection, alignItems: 'flex-start' }}>
+                  <View style={styles.columnSection}>
+                    <AppText style={styles.label}>Script</AppText>
+                    <AppText style={{...styles.valueSmall, marginTop: 2 }}>{this.getScriptLongName(currentAccount.scriptVersion as ScriptVersion)}</AppText>
+                    <AppText style={styles.valueSmall}>({this.getScriptShortName(currentAccount.scriptVersion as ScriptVersion)})</AppText>
+                  </View>
+                  <View style={styles.columnSection}>
+                    <AppText style={styles.label}>Fingerprint</AppText>
+                    <AppText style={{...styles.valueSmall, marginTop: 2 }}>{currentAccount?.fingerprint}</AppText>
+                  </View>
+              </View>
+            </View>
+
+            <Separator />
+
+            <View style={{...styles.columnSection, height: 152, marginTop: 10 }}>
+              <View style={styles.columnSection}>
+                <View style={{
+                  display: 'flex',
+                  flexDirection: 'row',
+                  alignItems: 'baseline'
+                }}>
+                  <AppText style={styles.label}>
+                    Searching derivation path
+                  </AppText>
+                  { ! currentAccount.snapshot &&
+                      <EllipsisAnimation
+                        style={{ marginLeft: 5 }}
+                        size={2}
+                        color={Colors.grey79} />
+                  }
                 </View>
+                <AppText style={{...styles.valueLarge, marginTop: 8, letterSpacing: 5 }}>{currentAccount?.derivationPath}</AppText>
               </View>
-
-              <Separator />
-
-              <View style={{...styles.columnSection, height: 152, marginTop: 10 }}>
-                <View style={styles.columnSection}>
-                  <View style={{
-                    display: 'flex',
-                    flexDirection: 'row',
-                    alignItems: 'baseline'
-                  }}>
-                    <AppText style={styles.label}>
-                      Searching derivation path
-                    </AppText>
-                    { ! currentAccount.snapshot &&
+              <View style={{...styles.rowSection, alignItems: 'flex-start', marginTop: 30 }}>
+                  <View style={styles.columnSection}>
+                    <AppText style={styles.label}>Found UTXOs</AppText>
+                    {
+                      currentAccount.snapshot ?
+                        <AppText style={{...styles.valueLarge, marginTop: 8}}>
+                          {numFormat(currentAccount?.snapshot?.numUtxos)}
+                        </AppText>
+                        :
                         <EllipsisAnimation
-                          style={{ marginLeft: 5 }}
-                          size={2}
-                          color={Colors.grey79} />
+                          style={{height: 20}}
+                          size={3}
+                          color={Colors.grey107} />
                     }
                   </View>
-                  <AppText style={{...styles.valueLarge, marginTop: 8, letterSpacing: 5 }}>{currentAccount?.derivationPath}</AppText>
-                </View>
-                <View style={{...styles.rowSection, alignItems: 'flex-start', marginTop: 30 }}>
-                    <View style={styles.columnSection}>
-                      <AppText style={styles.label}>Found UTXOs</AppText>
-                      {
-                        currentAccount.snapshot ?
-                          <AppText style={{...styles.valueLarge, marginTop: 8}}>
-                            {numFormat(currentAccount?.snapshot?.numUtxos)}
-                          </AppText>
-                          :
-                          <EllipsisAnimation
-                            style={{height: 20}}
-                            size={3}
-                            color={Colors.grey107} />
-                      }
-                    </View>
-                    <View style={styles.columnSection}>
-                      <AppText style={styles.label}>Total spendable sats</AppText>
-                      {
-                        currentAccount.snapshot ?
-                          <AppText style={{...styles.valueLarge, marginTop: 8}}>
-                            {numFormat(currentAccount?.snapshot?.balanceSats)}
-                          </AppText>
-                          :
-                          <EllipsisAnimation
-                            style={{height: 20}}
-                            size={3}
-                            color={Colors.grey107} />
-                      }
-                    </View>
-                </View>
+                  <View style={styles.columnSection}>
+                    <AppText style={styles.label}>Total spendable sats</AppText>
+                    {
+                      currentAccount.snapshot ?
+                        <AppText style={{...styles.valueLarge, marginTop: 8}}>
+                          {numFormat(currentAccount?.snapshot?.balanceSats)}
+                        </AppText>
+                        :
+                        <EllipsisAnimation
+                          style={{height: 20}}
+                          size={3}
+                          color={Colors.grey107} />
+                    }
+                  </View>
               </View>
-
-              <Button
-                title='Close'
-                onPress={() => this.props.onClose()}
-                style={styles.closeButton}
-              ></Button>
-            </LinearGradient>
-          </View>
+            </View>
+          </ModalDialog>
         )}
       </AccountsContext.Consumer>
-        
     );
   }
 
 }
 
 const styles = StyleSheet.create({
-  dimmedOverlay: {
-    flex: 1,
-    flexDirection: 'column',
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: 'rgba(0, 0, 0, 0.65)',
-    ...Layout.container.horizontalPadded
-  },
-  dialogBackground: {
-    display: 'flex',
-    flexDirection: 'column',
-    justifyContent: 'flex-start',
-    alignItems: 'center',
-    width: '100%',
-    borderRadius: 3,
-  },
   heading: {
     ...Typography.fontFamily.sfProDisplayMedium,
     color: Colors.highlight,
@@ -211,12 +184,5 @@ const styles = StyleSheet.create({
     justifyContent: 'space-evenly',
     alignItems: 'center',
     width: '100%'
-  },
-  closeButton: {
-    backgroundColor: 'rgba(255, 255, 255, 0.11)',
-    color: Colors.actionText,
-    width: '100%',
-    marginBottom: 0,
-    borderRadius: 0
-  },
+  }
 });
