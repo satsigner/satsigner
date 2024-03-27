afe942c9df5c9c13bc84e9fe2f91bf4d97277b45
Carlos Marques <psycarlo1@gmail.com>
2024-03-27 14:36:06 +0000

    chore: add bdk with example

--- apps/mobile/app/index.tsx
+++ apps/mobile/app/index.tsx
@@ -1,10 +1,22 @@
 import { StatusBar } from 'expo-status-bar'
-import { StyleSheet, Text, View } from 'react-native'
+import { useState } from 'react'
+import { StyleSheet, Text, View, Button } from 'react-native'
+import { Mnemonic } from 'bdk-rn'
+import { WordCount } from 'bdk-rn/lib/lib/enums'
 
 export default function App() {
+  const [test, setTest] = useState("")
+
+  const handleClick = async () => {
+    const mnemonic = await new Mnemonic().create(WordCount.WORDS12)
+    setTest(mnemonic.asString())
+  }
+
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
+      <Button title="Generate" onPress={handleClick} />
+      <Text>{test}</Text>
       <StatusBar style="auto" />
     </View>
   )
