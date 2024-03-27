d3f86b5038b099962da8117c5d6e2dcda53a63c5
Carlos Marques <psycarlo1@gmail.com>
2024-03-27 12:00:49 +0000

    chore: init

--- src/components/shared/usePrevious.ts
+++
@@ -1,9 +0,0 @@
-import { useRef, useEffect } from 'react';
-
-export default function usePrevious(value: any) {
-  const ref = useRef();
-  useEffect(() => {
-    ref.current = value; //assign the value of ref to the argument
-  },[value]); //this code will run when the value of 'value' changes
-  return ref.current; //in the end, return the current ref value.
-}
