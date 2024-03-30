93d4f286e6a9306d8553d299b70f4fb48e048d83
tmakerman <116613158+tmakerman@users.noreply.github.com>
2024-03-29 23:27:25 -0400

    Implements action bar layout and styling.

--- src/components/accounts/AccountTransactionsScreen.tsx
+++ src/components/accounts/AccountTransactionsScreen.tsx
@@ -2,7 +2,8 @@ import { useEffect, useContext } from 'react';
 import {
   View,
   StyleSheet,
-  ScrollView
+  ScrollView,
+  TouchableHighlight
 } from 'react-native';
 
 import { NavigationProp } from '@react-navigation/native';
@@ -14,6 +15,10 @@ import { AppText } from '../shared/AppText';
 import { AccountsContext } from './AccountsContext';
 import numFormat from '../../utils/numFormat';
 import BackgroundGradient from '../shared/BackgroundGradient';
+import LinearGradient from 'react-native-linear-gradient';
+
+import CameraIcon from '../../assets/images/camera.svg';
+import notImplementedAlert from '../shared/NotImplementedAlert';
 
 interface Props {
   navigation: NavigationProp<any>;
@@ -28,6 +33,28 @@ export default function AccountTransactionsScreen({
     navUtils.setHeaderTitle(context.currentAccount.name, navigation);
   }, []);
 
+  const GradientSeparator = () => <LinearGradient
+    style={{width: '100%', height: 1}}
+    colors={[Colors.grey61, Colors.grey38]}
+    start={{x: 0, y: 0}}
+    end={{x: 1.0, y: 0}}
+  />;
+
+  const ActionButton = (props: any) => <TouchableHighlight
+    activeOpacity={0.65}
+    underlayColor={Colors.grey38}
+    style={[{
+      width: '40%',
+      height: '100%',
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center'
+    }, props.style]}
+    onPress={props.onPress}
+  >
+    {props.children}
+  </TouchableHighlight>;
+
   return (
     <AccountsContext.Consumer>
       {({currentAccount: account}) => (
@@ -37,12 +64,36 @@ export default function AccountTransactionsScreen({
               <View style={styles.currency}><AppText style={styles.sats}>{numFormat(account?.snapshot?.balanceSats)}</AppText><AppText style={styles.satsLabel}>sats</AppText></View>
               <View style={styles.currency}><AppText style={styles.usd}>{numFormat(account?.snapshot?.balanceUsd, 2)}</AppText><AppText style={styles.usdLabel}>USD</AppText></View>
             </View>
+            <GradientSeparator />
             <View style={styles.actionBar}>
-              <AppText>Actions</AppText>
+              <ActionButton style={{
+                  borderRightWidth: 1,
+                  borderRightColor: Colors.grey48,                
+                }}
+                onPress={notImplementedAlert}
+              >
+                <AppText style={styles.actionLabel}>Sign & Send</AppText>
+              </ActionButton>
+              <ActionButton
+                style={{width: '20%'}}
+                onPress={notImplementedAlert}
+              >
+                <CameraIcon width={18} height={13} />
+              </ActionButton>
+              <ActionButton style={{
+                  borderLeftWidth: 1,
+                  borderLeftColor: Colors.grey48,                
+                }}
+                onPress={notImplementedAlert}
+              >
+                <AppText style={styles.actionLabel}>New Invoice</AppText>
+              </ActionButton>
             </View>
+            <GradientSeparator />
             <View style={styles.tabs}>
               <AppText>Tabs</AppText>
             </View>
+            <GradientSeparator />
           </BackgroundGradient>
           <View style={styles.transactionsHeader}>
             <AppText>Transactions Header</AppText>
@@ -92,14 +143,18 @@ const styles = StyleSheet.create({
     marginLeft: 3
   },
   actionBar: {
-    height: 75,
-    borderTopWidth: 1,
-    borderBottomWidth: 1
+    height: 60,
+    backgroundColor: 'rgba(255, 255, 255, 0.03)',
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-evenly',
+    alignItems: 'center'
+  },
+  actionLabel: {
+    ...Typography.capitalization.uppercase
   },
   tabs: {
     height: 75,
-    borderBottomColor: 'white',
-    borderBottomWidth: 1
   },
   transactionsHeader: {
     height: 75,
