b02125ecfaf28110f823312138dddc52a690ded2
Fahad-Mahmood <fahad492@outlook.com>
2024-04-07 22:13:27 +0500

    config: navigation screen types added

---
+++ src/types/navigation.d.ts
@@ -0,0 +1,9 @@
+type RootStackParamList = {
+  Home: undefined;
+  AccountList: undefined;
+  CreateParentAccount: undefined;
+  AccountOptions: undefined;
+  ImportSeed: undefined;
+  GenerateSeed: undefined;
+  ConfirmWord: undefined;
+};
