f67cf8c89a8f3dd52b69cf9761e2e6814b935d35
tmakerman <116613158+tmakerman@users.noreply.github.com>
2024-04-07 22:20:24 -0400

    Add AccountTransactions to RootStackParamList

--- src/types/navigation.d.ts
+++ src/types/navigation.d.ts
@@ -1,6 +1,7 @@
 type RootStackParamList = {
   Home: undefined;
   AccountList: undefined;
+  AccountTransactions: undefined;
   CreateParentAccount: undefined;
   AccountOptions: undefined;
   ImportSeed: undefined;
