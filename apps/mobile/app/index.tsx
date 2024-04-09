f3b6ba75b5a7f3f1fc32287b4613c4d5789efcc7
Carlos Marques <psycarlo1@gmail.com>
2024-04-10 00:31:01 +0100

    chore: add icons

--- apps/mobile/app/index.tsx
+++ apps/mobile/app/index.tsx
@@ -4,6 +4,7 @@ import { StyleSheet, Text, View, Button } from 'react-native'
 import { Mnemonic } from 'bdk-rn'
 import { WordCount } from 'bdk-rn/lib/lib/enums'
 import { i18n } from '@/locales'
+import { Image } from 'expo-image'
 
 export default function App() {
   const [test, setTest] = useState('')
@@ -19,6 +20,10 @@ export default function App() {
         {i18n.t('satsigner.name')}
       </Text>
       <Button title="Generate" onPress={handleClick} />
+      <Image
+        source={require('@/assets/icons/settings.svg')}
+        style={{ width: 25, height: 25 }}
+      />
       <Text>{test}</Text>
       <StatusBar style="auto" />
     </View>
