99217b1980d61dc749b6b0491852320afa83a745
Carlos Marques <psycarlo1@gmail.com>
2024-04-14 22:34:04 +0100

    chore: init docs with nextra

---
+++ apps/docs/theme.config.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { DocsThemeConfig } from 'nextra-theme-docs'
+
+const config: DocsThemeConfig = {
+  logo: <span>SatSigner</span>,
+  project: {
+    link: 'https://github.com/satsigner/satsigner'
+  },
+  docsRepositoryBase: 'https://github.com/satsigner/satsigner/apps/docs',
+  feedback: {
+    content: null
+  },
+  footer: {
+    text: 'SatSigner'
+  }
+}
+
+export default config
