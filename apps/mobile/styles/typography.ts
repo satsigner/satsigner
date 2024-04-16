b7f512078b4b85dd7d52a322affeb4eedf047281
Carlos Marques <psycarlo1@gmail.com>
2024-04-16 16:56:25 +0100

    chore: add typography

---
+++ apps/mobile/styles/typography.ts
@@ -0,0 +1,4 @@
+export const sfProTextLight = 'SF Pro Text Light'
+export const sfProTextRegular = 'SF Pro Text Regular'
+export const sfProTextMedium = 'SF Pro Text Medium'
+export const sfProTextBold = 'SF Pro Text Bold'
