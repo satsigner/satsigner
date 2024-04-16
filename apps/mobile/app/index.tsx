b3bff0cc9eb7534e71428e19caeded7c8bd3c0ef
Carlos Marques <psycarlo1@gmail.com>
2024-04-16 20:05:22 +0100

    chore: update index.tsx

--- apps/mobile/app/index.tsx
+++ apps/mobile/app/index.tsx
@@ -1,40 +1,17 @@
-import { StatusBar } from 'expo-status-bar'
-import { useState } from 'react'
-import { StyleSheet, Text, View, Button } from 'react-native'
-import { Mnemonic } from 'bdk-rn'
-import { WordCount } from 'bdk-rn/lib/lib/enums'
-import { i18n } from '@/locales'
-import { Image } from 'expo-image'
+import { StyleSheet, View } from 'react-native'
+import SSButton from '@/components/SSButton'
+import { useRouter } from 'expo-router'
+import SSMainLayout from '@/layouts/SSMainLayout'
 
 export default function App() {
-  const [test, setTest] = useState('')
-
-  const handleClick = async () => {
-    const mnemonic = await new Mnemonic().create(WordCount.WORDS12)
-    setTest(mnemonic.asString())
-  }
+  const router = useRouter()
 
   return (
-    <View style={styles.container}>
-      <Text style={{ fontFamily: 'SF Pro Text Bold' }}>
-        {i18n.t('satsigner.name')}
-      </Text>
-      <Button title="Generate" onPress={handleClick} />
-      <Image
-        source={require('@/assets/icons/settings.svg')}
-        style={{ width: 25, height: 25 }}
+    <SSMainLayout>
+      <SSButton
+        label="Account List"
+        onPress={() => router.push('/accountList/')}
       />
-      <Text>{test}</Text>
-      <StatusBar style="auto" />
-    </View>
+    </SSMainLayout>
   )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center'
-  }
-})
