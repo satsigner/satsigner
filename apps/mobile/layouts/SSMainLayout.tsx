738a5bee1f409cd009ccd332dbe9748725967567
Carlos Marques <psycarlo1@gmail.com>
2024-04-19 16:45:16 +0100

    chore: SSMainLayout black prop

--- apps/mobile/layouts/SSMainLayout.tsx
+++ apps/mobile/layouts/SSMainLayout.tsx
@@ -1,13 +1,19 @@
+import { useMemo } from 'react'
 import { StyleSheet, View } from 'react-native'
 
-import { Layout } from '@/styles'
+import { Colors, Layout } from '@/styles'
 
 type SSMainLayoutProps = {
+  black?: boolean
   children: React.ReactNode
 }
 
-export default function SSMainLayout({ children }: SSMainLayoutProps) {
-  return <View style={styles.containerBase}>{children}</View>
+export default function SSMainLayout({ black, children }: SSMainLayoutProps) {
+  const containerStyle = useMemo(() => {
+    return { backgroundColor: black ? Colors.black : Colors.gray[950] }
+  }, [black])
+
+  return <View style={[styles.containerBase, containerStyle]}>{children}</View>
 }
 
 const styles = StyleSheet.create({
