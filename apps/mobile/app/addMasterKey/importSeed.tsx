9ead00690307aee104545556d6918af6b1a16d36
Carlos Marques <psycarlo1@gmail.com>
2024-04-24 00:18:54 +0100

    chore: importSeed progress

--- apps/mobile/app/addMasterKey/importSeed.tsx
+++ apps/mobile/app/addMasterKey/importSeed.tsx
@@ -1,9 +1,12 @@
 import { Stack, useRouter } from 'expo-router'
+import { useState } from 'react'
 import { ScrollView } from 'react-native'
 
 import SSButton from '@/components/SSButton'
 import SSChecksumStatus from '@/components/SSChecksumStatus'
 import SSFingerprint from '@/components/SSFingerprint'
+import SSGradientModal from '@/components/SSGradientModal'
+import SSSeparator from '@/components/SSSeparator'
 import SSText from '@/components/SSText'
 import SSTextInput from '@/components/SSTextInput'
 import SSWordInput from '@/components/SSWordInput'
@@ -14,10 +17,21 @@ import SSSeedLayout from '@/layouts/SSSeedLayout'
 import SSVStack from '@/layouts/SSVStack'
 import { i18n } from '@/locales'
 import { useAccountStore } from '@/store/accounts'
+import { Colors } from '@/styles'
 
 export default function ImportSeed() {
   const router = useRouter()
   const accountStore = useAccountStore()
+
+  const [accountAddedModalVisible, setAccountAddedModalVisible] = useState(true)
+
+  async function handleOnPressImportSeed() {
+    //
+  }
+
+  async function handleOnCloseAccountAddedModal() {
+    setAccountAddedModalVisible(false)
+  }
 
   return (
     <SSMainLayout>
@@ -61,7 +75,7 @@ export default function ImportSeed() {
           <SSButton
             label={i18n.t('addMasterKey.importExistingSeed.action')}
             variant="secondary"
-            onPress={() => {}}
+            onPress={() => handleOnPressImportSeed()}
           />
           <SSButton
             label={i18n.t('common.cancel')}
@@ -70,6 +84,75 @@ export default function ImportSeed() {
           />
         </SSVStack>
       </ScrollView>
+      <SSGradientModal
+        visible={accountAddedModalVisible}
+        onClose={() => handleOnCloseAccountAddedModal()}
+      >
+        <SSVStack style={{ marginVertical: 32, width: '100%' }}>
+          <SSVStack itemsCenter gap="xs">
+            <SSText color="white" size="2xl">
+              {accountStore.currentAccount.name}
+            </SSText>
+            <SSText color="muted" size="lg">
+              {i18n.t('addMasterKey.importExistingSeed.accountAdded')}
+            </SSText>
+          </SSVStack>
+          <SSSeparator />
+          <SSHStack justifyEvenly>
+            <SSVStack itemsCenter>
+              <SSText style={{ color: Colors.gray[500] }}>
+                {i18n.t('bitcoin.script')}
+              </SSText>
+              <SSText size="md" color="muted">
+                {i18n.t('bitcoin.script')} Name
+              </SSText>
+            </SSVStack>
+            <SSVStack itemsCenter>
+              <SSText style={{ color: Colors.gray[500] }}>
+                {i18n.t('bitcoin.fingerprint')}
+              </SSText>
+              <SSText size="md" color="muted">
+                62e407ad
+              </SSText>
+            </SSVStack>
+          </SSHStack>
+          <SSSeparator />
+          <SSVStack>
+            <SSVStack itemsCenter>
+              <SSText style={{ color: Colors.gray[500] }}>
+                {i18n.t(
+                  'addMasterKey.importExistingSeed.accountAddedModal.derivationPath'
+                )}
+              </SSText>
+              <SSText size="md" color="muted">
+                m/84'/1'/0'
+              </SSText>
+            </SSVStack>
+            <SSHStack justifyEvenly>
+              <SSVStack itemsCenter>
+                <SSText style={{ color: Colors.gray[500] }}>
+                  {i18n.t(
+                    'addMasterKey.importExistingSeed.accountAddedModal.utxos'
+                  )}
+                </SSText>
+                <SSText size="md" color="muted">
+                  0
+                </SSText>
+              </SSVStack>
+              <SSVStack itemsCenter>
+                <SSText style={{ color: Colors.gray[500] }}>
+                  {i18n.t(
+                    'addMasterKey.importExistingSeed.accountAddedModal.sats'
+                  )}
+                </SSText>
+                <SSText size="md" color="muted">
+                  0
+                </SSText>
+              </SSVStack>
+            </SSHStack>
+          </SSVStack>
+        </SSVStack>
+      </SSGradientModal>
     </SSMainLayout>
   )
 }
