97839b9f100e8cb1904a1a0f8e4735a24b0fba4d
Carlos Marques <psycarlo1@gmail.com>
2024-04-23 19:30:49 +0100

    chore: update account store

--- apps/mobile/store/accounts.tsx
+++ apps/mobile/store/accounts.tsx
@@ -1,15 +1,28 @@
 import { create } from 'zustand'
 
+import { generateMnemonic } from '@/api/bdk'
 import { type Account } from '@/types/models/Account'
 
-interface AccountsState {
+type AccountsState = {
   accounts: Account[]
   currentAccount: Account
 }
 
-const useAccountStore = create<AccountsState>()(() => ({
+type AccountsAction = {
+  generateMnemonic: (
+    count: NonNullable<Account['seedWordCount']>
+  ) => Promise<void>
+}
+
+const useAccountStore = create<AccountsState & AccountsAction>()((set) => ({
   accounts: [],
-  currentAccount: { name: '', accountCreationType: null }
+  currentAccount: { name: '', accountCreationType: null },
+  generateMnemonic: async (count) => {
+    const mnemonic = await generateMnemonic(count)
+    set((state) => ({
+      currentAccount: { ...state.currentAccount, seedWords: mnemonic }
+    }))
+  }
 }))
 
 export { useAccountStore }
