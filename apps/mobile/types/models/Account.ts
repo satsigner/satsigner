3880b809f2c23e50c31dcd9cd2bdcd15b6c40e6e
Carlos Marques <psycarlo1@gmail.com>
2024-04-24 00:50:57 +0100

    chore: model types

--- apps/mobile/types/models/Account.ts
+++ apps/mobile/types/models/Account.ts
@@ -1,3 +1,6 @@
+import { Transaction } from './Transaction'
+import { Utxo } from './Utxo'
+
 export type Account = {
   name: string
   accountCreationType:
@@ -11,4 +14,17 @@ export type Account = {
   seedWords?: string[]
   passphrase?: string
   scriptVersion?: 'P2PKH' | 'P2SH-P2WPKH' | 'P2WPKH' | 'P2TR'
+  external_descriptor?: string
+  internal_descriptor?: string
+  fingerprint?: string
+  derivationPath?: string
+  transactions: Transaction[]
+  utxos: Utxo[]
+  summary: {
+    balance: number
+    numberOfAddresses: number
+    numberOfTransactions: number
+    numberOfUtxos: number
+    satsInMempool: number
+  }
 }
