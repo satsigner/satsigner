d4bb43e76fb72e9f544e99e29cb7deebef7d669a
tmakerman <116613158+tmakerman@users.noreply.github.com>
2024-05-02 23:22:40 -0400

    feature: layout and styling of UTXO item

--- src/screens/AccountUtxoList/components/UtxoItem.tsx
+++ src/screens/AccountUtxoList/components/UtxoItem.tsx
@@ -5,7 +5,10 @@ import { Utxo } from "../../../models/Utxo";
 
 import { AppText } from "../../../components/shared/AppText";
 import { TransactionBuilderContext } from "../../../components/accounts/TransactionBuilderContext";
-import { Colors } from "../../../styles";
+import { Colors, Layout } from "../../../styles";
+import { Sats } from "../../../components/accounts/Sats";
+import formatAddress from "../../../utils/formatAddress";
+import formatDate from "../../../utils/formatDate";
 
 interface Props {
   utxo: Utxo;
@@ -31,17 +34,100 @@ export default function UtxoItem({
 
   return (
     <TouchableOpacity onPress={onToggleSelected}>
-      <View style={selected ? styles.selected : {}}>
-        <AppText>{utxo.txid}:{utxo.vout}</AppText>
+      <View style={styles.container}>
+        <View style={styles.selectAction}>
+          <View style={styles.selectButton}></View>
+        </View>
+        <View style={styles.detailsContainer}>
+          <View style={styles.details}>
+            <View style={styles.detailsLeftColumn}>
+              <Sats
+                sats={utxo.value}
+                currencyStyle={styles.currency}
+                satsStyle={styles.sats}
+                satsLabelStyle={styles.satsLabel}
+                usdStyle={styles.usd}
+                usdLabelStyle={styles.usdLabel}
+              ></Sats>
+            </View>
+            <View style={styles.detailsRightColumn}>
+              <AppText style={styles.address}>{formatAddress(utxo.addressTo)}</AppText>
+              <AppText style={styles.date}>{formatDate(utxo.timestamp)}</AppText>
+            </View>
+          </View>
+          <AppText style={styles.memo}>{ utxo.label && 'Memo: '}{utxo.label}</AppText>
+        </View>
       </View>
     </TouchableOpacity>
   );
 }
 
 const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    backgroundColor: Colors.grey27,
+    borderTopWidth: 2,
+    borderTopColor: Colors.grey42,
+    borderStyle: 'solid',
+    ...Layout.container.horizontalPaddedThin,
+    paddingVertical: 15
+  },
+  selectAction: {
+    paddingTop: 3,
+    width: '10%',
+    // opacity: 0.56,
+  },
+  selectButton: {
+    backgroundColor: Colors.grey79,
+    height: 20,
+    width: 20,
+    borderRadius: 10
+  },
+  detailsContainer: {
+    width: '90%',
+  },
+  details: {
+    flexDirection: 'row',
+    justifyContent: 'space-between'
+  },
+  detailsLeftColumn: {
+
+  },
+  detailsRightColumn: {
+    alignItems: 'flex-end'
+  },
   selected: {
-    padding: 5,
-    margin: 5,
-    backgroundColor: Colors.red4
+  },
+  currency: {
+    marginBottom: 5
+  },
+  sats: {
+    fontSize: 14,
+    color: Colors.white
+  },
+  satsLabel: {
+    fontSize: 10,
+    color: Colors.grey130,
+  },
+  usd: {
+    fontSize: 11,
+    color: Colors.white
+  },
+  usdLabel: {
+    fontSize: 11,
+    color: Colors.grey130
+  },
+  memo: {
+    color: Colors.white,
+    fontSize: 11
+  },
+  address: {
+    color: Colors.white,
+    fontSize: 12
+  },
+  date: {
+    color: Colors.grey189,
+    fontSize: 12
   }
 });
