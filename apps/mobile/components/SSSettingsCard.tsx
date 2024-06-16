0ff1995bb295d5409b93de6f4dcd599e3d4620aa
Carlos Marques <psycarlo1@gmail.com>
2024-06-16 23:26:42 +0100

    chore: add SSSettingsCard

---
+++ apps/mobile/components/SSSettingsCard.tsx
@@ -0,0 +1,52 @@
+import { Image } from 'expo-image'
+import { TouchableOpacity, StyleSheet } from 'react-native'
+
+import SSHStack from '@/layouts/SSHStack'
+import SSVStack from '@/layouts/SSVStack'
+
+import SSText from './SSText'
+import { Colors } from '@/styles'
+
+type SSSettingsCardsProps = {
+  title: string
+  description: string
+  icon: React.ReactNode
+  onPress(): void
+}
+
+export default function SSSettingsCards({
+  title,
+  description,
+  icon,
+  onPress
+}: SSSettingsCardsProps) {
+  return (
+    <TouchableOpacity
+      activeOpacity={0.5}
+      style={styles.containerBase}
+      onPress={() => onPress()}
+    >
+      <SSHStack justifyBetween>
+        <SSHStack style={{ alignItems: 'center' }}>
+          {icon}
+          <SSVStack gap="none" style={{ maxWidth: '86%' }}>
+            <SSText size="lg">{title}</SSText>
+            <SSText color="muted">{description}</SSText>
+          </SSVStack>
+        </SSHStack>
+        <Image
+          style={{ width: 6, height: 11.6 }}
+          source={require('@/assets/icons/chevron-right.svg')}
+        />
+      </SSHStack>
+    </TouchableOpacity>
+  )
+}
+
+const styles = StyleSheet.create({
+  containerBase: {
+    borderTopWidth: 1,
+    borderTopColor: Colors.gray[850],
+    padding: 24
+  }
+})
