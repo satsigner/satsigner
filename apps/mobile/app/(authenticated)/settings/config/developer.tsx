9d00dd461c5f54e2fb952ff628ab3a41f8b71862
KitchenMishap <hello@kitchenmishap.art>
2024-12-16 10:27:25 +0000

    Developer page has button to show some direct BDK function call results (on Blockchain object)

--- apps/mobile/app/(authenticated)/settings/config/developer.tsx
+++ apps/mobile/app/(authenticated)/settings/config/developer.tsx
@@ -10,17 +10,50 @@ import { i18n } from '@/locales'
 import { useAccountsStore } from '@/store/accounts'
 import { useAuthStore } from '@/store/auth'
 
+import { useBlockchainStore } from '@/store/blockchain'
+import { Blockchain } from 'bdk-rn'
+import { FeeRate } from 'bdk-rn/lib/classes/Bindings'
+
 export default function Developer() {
   const deleteAccounts = useAccountsStore((state) => state.deleteAccounts)
   const setFirstTime = useAuthStore((state) => state.setFirstTime)
+  const getBlockChain = useBlockchainStore((state) => state.getBlockchain)
 
   const [deletingAccounts, setDeletingAccounts] = useState(false)
+  const [gettingBdkInfo, setGettingBdkInfo] = useState(false)
+  const [consoleTxt, setConsoleTxt] = useState(['...'])
 
   async function handleDeleteAccount() {
     setDeletingAccounts(true)
     await deleteAccounts()
     setDeletingAccounts(false)
     Alert.alert('Accounts deleted')
+  }
+
+  async function handleGetBdkInfo() {
+    setGettingBdkInfo(true)
+    setConsoleTxt(['<Loading>...'])
+    try {
+      const lines: [string] = ['METHODS ON BLOCKCHAIN OBJECT']
+      const blockchain: Blockchain = await getBlockChain()
+      const height: number = await blockchain.getHeight()
+      lines.push('Blockchain.getHeight():' + height.toString())
+      const feeFor4Confirmations: FeeRate = await blockchain.estimateFee(4)
+      lines.push(
+        'Blockchain.estimateFee(4).asSatPerVb(): ' +
+          feeFor4Confirmations.asSatPerVb().toFixed(3) +
+          ' sats/Vb'
+      )
+      const hashAtHeight0: string = await blockchain.getBlockHash(0)
+      lines.push('Blockchain.getBlockHash(0): ' + hashAtHeight0)
+      lines.push('Blockchain.broadcast(Transaction): Not called')
+      lines.push('')
+      setConsoleTxt(lines)
+      setGettingBdkInfo(false)
+    } catch (err: any) {
+      setConsoleTxt([err.message])
+      setGettingBdkInfo(false)
+    }
   }
 
   return (
@@ -46,6 +79,13 @@ export default function Developer() {
             label="Set PIN First Time"
             onPress={() => setFirstTime(true)}
           />
+          <SSButton
+            label="BDK info [BlockChain]"
+            loading={gettingBdkInfo}
+            onPress={() => handleGetBdkInfo()}
+          />
+          <SSText>
+            {consoleTxt.join('\n')}</SSText>
         </SSVStack>
       </SSMainLayout>
     </>
