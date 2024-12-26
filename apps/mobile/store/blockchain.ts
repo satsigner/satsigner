2d642147c876b577d496fa4c1edf2d4d6dc16f4e
v4v2 <tai@tilde.team>
2024-12-26 17:12:04 -0300

    ui: more options in bitcoin network settings

--- apps/mobile/store/blockchain.ts
+++ apps/mobile/store/blockchain.ts
@@ -11,12 +11,18 @@ type BlockchainState = {
   backend: Backend
   network: Network
   url: string
+  timeout: number
+  retries: number
+  stopGap: number
 }
 
 type BlockchainAction = {
   setBackend: (backend: Backend) => void
   setNetwork: (network: Network) => void
   setUrl: (url: string) => void
+  setTimeout: (timeout: number) => void
+  setStopGap: (stopGap: number) => void
+  setRetries: (retries: number) => void
   getBlockchain: () => Promise<Blockchain>
   getBlockchainHeight: () => Promise<number>
 }
@@ -27,6 +33,9 @@ const useBlockchainStore = create<BlockchainState & BlockchainAction>()(
       backend: 'esplora',
       network: 'signet',
       url: ESPLORA_MUTINYNET_URL,
+      timeout: 6,
+      retries: 7,
+      stopGap: 20,
       setBackend: (backend) => {
         set({ backend })
       },
@@ -35,6 +44,15 @@ const useBlockchainStore = create<BlockchainState & BlockchainAction>()(
       },
       setUrl: (url) => {
         set({ url })
+      },
+      setTimeout: (timeout) => {
+        set({ timeout })
+      },
+      setStopGap: (stopGap) => {
+        set({ stopGap })
+      },
+      setRetries: (retries) => {
+        set({ retries })
       },
       getBlockchain: async () => {
         const config = getBlockchainConfig(get().backend, get().url)
