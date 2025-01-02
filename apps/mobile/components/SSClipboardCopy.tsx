2e8c462a877b390e694519088326fb1f940cfc59
v4v2 <tai@tilde.team>
2025-01-02 10:23:50 -0300

    feat: add component to copy any text to clipboard upon user click

---
+++ apps/mobile/components/SSClipboardCopy.tsx
@@ -0,0 +1,34 @@
+import { TouchableWithoutFeedback, View } from "react-native";
+import * as Clipboard from 'expo-clipboard'
+import SSPopupText from "./SSPopupText";
+import { useState } from "react";
+
+type SSTextClipboardProps = {
+  text: string|number
+  children: React.ReactNode
+}
+
+export default function SSTextClipboard({
+  text, children
+}: SSTextClipboardProps) {
+
+  const [showPopup, setShowPopup] = useState(false)
+  const handleClick = async () => {
+    const textToCopy = typeof text === 'string' ? text : text.toString();
+    await Clipboard.setStringAsync(textToCopy)
+    setShowPopup(true)
+  }
+
+  return (
+    <TouchableWithoutFeedback onPress={handleClick}>
+      <View>
+        {children}
+        <SSPopupText
+          isVisible={showPopup}
+          onTimeout={() => setShowPopup(false)}
+          message="Copied to clipboard"
+        />
+      </View>
+    </TouchableWithoutFeedback>
+  )
+}
