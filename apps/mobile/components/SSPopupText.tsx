730d57bc3eff61aade58f320fee4dc45cda9d1c3
v4v2 <tai@tilde.team>
2025-01-02 10:22:54 -0300

    feat: add PopupText component for showing floating text

---
+++ apps/mobile/components/SSPopupText.tsx
@@ -0,0 +1,54 @@
+import React, { useEffect } from "react";
+import { View, StyleSheet } from "react-native";
+import SSText from "./SSText";
+
+type SSSnackbarProps = {
+  message: string
+  onTimeout: () => void
+  isVisible: boolean
+  duration?: number
+}
+
+export default function SSPopupText({
+  message,
+  isVisible,
+  onTimeout,
+  duration = 600,
+}: SSSnackbarProps) {
+
+  useEffect(() => {
+    if (isVisible) {
+      const timeout = setTimeout(() => {
+        onTimeout()
+      }, duration);
+      return () => clearTimeout(timeout);
+    }
+  }, [isVisible, duration]);
+
+  return isVisible ? (
+    <View style={styles.container} >
+      <SSText style={styles.messageText} >
+        {message}
+      </SSText>
+    </View>
+  ) : null
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+    position: "absolute",
+    height: '100%',
+    width: '100%',
+  },
+  messageText: {
+    backgroundColor: 'grey',
+    opacity: 0.75,
+    borderRadius: 5,
+    paddingHorizontal: 5,
+    paddingVertical: 0,
+    width: 'auto'
+  },
+});
