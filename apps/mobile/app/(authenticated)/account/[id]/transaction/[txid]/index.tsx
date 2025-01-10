58b3bbe1b541c787f2ccacb1055c5d89c8df4c4b
v4v2 <tai@tilde.team>
2025-01-10 10:28:00 -0300

    ui: tx details improvements

--- apps/mobile/app/(authenticated)/account/[id]/transaction/[txid]/index.tsx
+++ apps/mobile/app/(authenticated)/account/[id]/transaction/[txid]/index.tsx
@@ -21,6 +21,8 @@ import {
 } from '@/utils/format'
 import { useEffect, useState } from 'react'
 import { Transaction } from '@/types/models/Transaction'
+import { SSIconEdit, SSIconIncoming, SSIconOutgoing } from '@/components/icons'
+import { Colors } from '@/styles'
 
 export default function TxDetails() {
   const { id: accountId, txid } = useLocalSearchParams<TxSearchParams>()
@@ -38,19 +40,71 @@ export default function TxDetails() {
     state.getTx
   ])
 
-  const placeholder = '-'
   const [tx, setTx] = useState({} as Transaction)
   const [tags, setLocalTags] = useState(getTags())
   const [selectedTags, setSelectedTags] = useState([] as string[])
   const [label, setLabel] = useState('')
   const [originalLabel, setOriginalLabel] = useState('')
 
+  const placeholder = () => useState('-');
+
+  const [amount, setAmount] = placeholder()
+  const [fee, setFee] = placeholder()
+  const [feePerByte, setFeePerByte] = placeholder()
+  const [feePerVByte, setFeePerVByte] = placeholder()
+  const [height, setHeight] = placeholder()
+  const [oldPrice, setOldPrice] = placeholder()
+  const [price, setPrice] = placeholder()
+  const [raw, setRaw] = placeholder()
+  const [size, setSize] = placeholder()
+  const [type, setType] = placeholder()
+  const [version, setVersion] = placeholder()
+  const [vsize, setVsize] = placeholder()
+  const [weight, setWeight] = placeholder()
+
   const fetchTxInfo = () => {
     const tx = getTx(accountId, txid)
 
     if (!tx) return
 
+    const amount = (tx.type === 'receive') ? tx.received : tx.sent
+
     setTx(tx)
+    setType(tx.type)
+    setAmount(formatNumber(amount))
+
+    if (btcPrice)
+      setPrice(formatFiatPrice(Number(amount), btcPrice))
+
+    if (tx.prices?.[fiatCurrency])
+      setOldPrice(formatFiatPrice(Number(amount), tx.prices[fiatCurrency]))
+
+    if (tx.blockHeight)
+      setHeight(tx.blockHeight.toString())
+
+    if (tx.size)
+      setSize(tx.size.toString())
+
+    if (tx.vsize)
+      setVsize(tx.vsize.toString())
+
+    if (tx.weight)
+      setWeight(tx.weight.toString())
+
+    if (tx.fee)
+      setFee(tx.fee.toString())
+
+    if (tx.fee && tx.size)
+      setFeePerByte(formatNumber(tx.fee/tx.size))
+
+    if (tx.fee && tx.vsize)
+      setFeePerVByte(formatNumber(tx.fee/tx.vsize))
+
+    if (tx.version)
+      setVersion(tx.version.toString())
+
+    if (tx.raw)
+      setRaw(tx.raw.map(v => v.toString(16).padStart(2, '0')).join(' '))
 
     const rawLabel = tx.label || ''
     const { label, tags } = formatLabel(rawLabel)
@@ -67,8 +121,8 @@ export default function TxDetails() {
 
     if (selectedTags.length > 0) newLabel += ' tags:' + selectedTags.join(',')
 
-    if (newLabel !== originalLabel)
-      // setUtxoLabel(accountId, txid, Number(vout), newLabel)
+    // if (newLabel !== originalLabel)
+    // setUtxoLabel(accountId, txid, Number(vout), newLabel)
 
       router.back()
   }
@@ -105,83 +159,117 @@ export default function TxDetails() {
           padding: 20
         }}
       >
-        {tx && (
-          <SSVStack gap="none">
-            <SSText center>
-              {formatNumber(tx.sent || tx.received || 0) + ' sats'}
+        <SSVStack gap="none" style={{ alignItems: 'center' }}>
+          <SSHStack gap="xxs" style={{ alignItems: 'baseline', width: 'auto' }}>
+            <SSText size="4xl" style={{ lineHeight: 30 }}>
+              {amount}
             </SSText>
-            <SSText center>
-              {formatFiatPrice(tx.sent || tx.received, btcPrice)}
-              {fiatCurrency}
+            <SSText color="muted">
+              {i18n.t('bitcoin.sats').toLowerCase()}
             </SSText>
-            <SSText center>{formatDate(tx.timestamp)}</SSText>
-            {tx.prices && (
-              <SSText center>
-                (
-                {formatFiatPrice(
-                  tx.sent || tx.received,
-                  tx.prices[fiatCurrency]
-                )}
-                ){fiatCurrency}
-              </SSText>
-            )}
-          </SSVStack>
-        )}
-        <SSVStack gap="none">
-          <SSText>IN BLOCK</SSText>
-          <SSText>{tx?.blockHeight || placeholder}</SSText>
-        </SSVStack>
-        <SSVStack gap="none">
-          <SSText>TRANSACTION HASH</SSText>
-          <SSText>{txid}</SSText>
-        </SSVStack>
-        <SSHStack>
-          <SSVStack gap="none" style={{ width: '33%' }}>
-            <SSText>RAW SIZE</SSText>
-            <SSText>{tx?.size || placeholder} </SSText>
-          </SSVStack>
-          <SSVStack gap="none" style={{ width: '33%' }}>
-            <SSText>WEIGHT</SSText>
-            <SSText>{tx?.weight || placeholder} </SSText>
-          </SSVStack>
-          <SSVStack gap="none" style={{ width: '33%' }}>
-            <SSText>VIRTUAL SIZE</SSText>
-            <SSText>{tx?.vsize || placeholder} </SSText>
-          </SSVStack>
-        </SSHStack>
-        <SSHStack>
-          <SSVStack gap="none" style={{ width: '33%' }}>
-            <SSText>FEES</SSText>
-            <SSText>{tx?.fee || placeholder} </SSText>
-          </SSVStack>
-          <SSVStack gap="none" style={{ width: '33%' }}>
-            <SSText>FEE SAT/B</SSText>
-            <SSText>
-              {tx?.fee && tx?.size
-                ? formatNumber(tx.fee / tx.size, 2) + ' sats'
-                : placeholder}
-            </SSText>
-          </SSVStack>
-          <SSVStack gap="none" style={{ width: '33%' }}>
-            <SSText>FEE SAT/VB</SSText>
-            <SSText>
-              {tx?.fee && tx?.vsize
-                ? formatNumber(tx.fee / tx.vsize, 2) + ' sats'
-                : placeholder}
-            </SSText>
-          </SSVStack>
-        </SSHStack>
-        <SSVStack gap="none">
-          <SSText>TRANSACTION RAW</SSText>
-          <SSText>
-            {tx?.raw
-              ? tx.raw.map((x) => x.toString(16).padStart(2, '0')).join(' ')
-              : placeholder}
+          </SSHStack>
+          <SSText color="muted">
+            {price} {fiatCurrency}
           </SSText>
+          <SSHStack gap="xs">
+           {type === 'receive' && (
+              <SSIconIncoming height={12} width={12} />
+            )}
+            {type === 'send' && (
+              <SSIconOutgoing height={12} width={12} />
+            )}
+            <SSText center color="muted">
+              {formatDate(tx.timestamp)}
+            </SSText>
+          </SSHStack>
+          {oldPrice !== '-' && (
+            <SSText center color="muted">
+              ({oldPrice}) {fiatCurrency}
+            </SSText>
+          )}
         </SSVStack>
+        <SSSeparator color="gradient" />
         <SSVStack gap="none">
-          <SSText>TRANSACTION VERSION</SSText>
-          <SSText>{tx?.version || placeholder}</SSText>
+          <SSHStack justifyBetween>
+            <SSText size="md">
+              {label || i18n.t('account.noLabel')}
+            </SSText>
+            <SSIconEdit
+              height={32}
+              width={32}
+            />
+          </SSHStack>
+          <SSHStack gap="sm">
+            {selectedTags.map((tag) => (
+              <SSButton
+                key={tag}
+                label={tag}
+                uppercase={false}
+                style={{
+                  backgroundColor: Colors.gray[500],
+                  borderRadius: 5,
+                  borderStyle: 'solid',
+                  paddingHorizontal: 8,
+                  height: 'auto',
+                  width: 'auto'
+                }}
+              />
+            ))}
+          </SSHStack>
+        </SSVStack>
+        <SSSeparator color="gradient" />
+        <SSClipboardCopy text={height}>
+          <SSVStack gap="none">
+            <SSText weight="bold" size="md">IN BLOCK</SSText>
+            <SSText color="muted">{height}</SSText>
+          </SSVStack>
+        </SSClipboardCopy>
+        <SSSeparator color="gradient" />
+        <SSClipboardCopy text={txid}>
+          <SSVStack gap="none">
+            <SSText weight="bold" size="md">TRANSACTION HASH</SSText>
+            <SSText color="muted">{txid}</SSText>
+          </SSVStack>
+        </SSClipboardCopy>
+        <SSSeparator color="gradient" />
+        <SSHStack>
+          <SSVStack gap="none" style={{ width: '33%' }}>
+            <SSText weight="bold" size="md">RAW SIZE</SSText>
+            <SSText color="muted">{size}</SSText>
+          </SSVStack>
+          <SSVStack gap="none" style={{ width: '33%' }}>
+            <SSText weight="bold" size="md">WEIGHT</SSText>
+            <SSText color="muted">{weight}</SSText>
+          </SSVStack>
+          <SSVStack gap="none" style={{ width: '33%' }}>
+            <SSText weight="bold" size="md">VIRTUAL SIZE</SSText>
+            <SSText color="muted">{vsize}</SSText>
+          </SSVStack>
+        </SSHStack>
+        <SSSeparator color="gradient" />
+        <SSHStack>
+          <SSVStack gap="none" style={{ width: '33%' }}>
+            <SSText weight="bold" size="md">FEES</SSText>
+            <SSText color="muted">{fee}</SSText>
+          </SSVStack>
+          <SSVStack gap="none" style={{ width: '33%' }}>
+            <SSText weight="bold" size="md">FEE SAT/B</SSText>
+            <SSText color="muted">{feePerByte}</SSText>
+          </SSVStack>
+          <SSVStack gap="none" style={{ width: '33%' }}>
+            <SSText weight="bold" size="md">FEE SAT/VB</SSText>
+            <SSText color="muted">{feePerVByte}</SSText>
+          </SSVStack>
+        </SSHStack>
+        <SSSeparator color="gradient" />
+        <SSVStack gap="none">
+          <SSText weight="bold" size="md">TRANSACTION RAW</SSText>
+          <SSText color="muted">{raw}</SSText>
+        </SSVStack>
+        <SSSeparator color="gradient" />
+        <SSVStack gap="none">
+          <SSText weight="bold" size="md">Version</SSText>
+          <SSText color="muted">{version}</SSText>
         </SSVStack>
       </SSVStack>
     </ScrollView>
