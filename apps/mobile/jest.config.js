c372295ee443cfa69e04ef2690c31d3092485dd8
Carlos Marques <psycarlo1@gmail.com>
2025-01-16 18:47:10 +0000

    chore: add todo

--- apps/mobile/jest.config.js
+++ apps/mobile/jest.config.js
@@ -6,7 +6,7 @@ const config = {
   preset: 'jest-expo',
   transformIgnorePatterns: [
     'node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|bdk-rn|react-native-svg)'
-  ]
+  ] // TODO: add bip21
 }
 
 module.exports = config
