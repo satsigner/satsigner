5686f03afad1fa33f581b13b8c421c474bd781f8
Carlos Marques <psycarlo1@gmail.com>
2025-02-10 14:13:29 +0000

    fix: move shim back

--- apps/mobile/app/_layout.tsx
+++ apps/mobile/app/_layout.tsx
@@ -1,5 +1,3 @@
-import '../../shim'
-
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { Slot } from 'expo-router'
 import { setStatusBarStyle } from 'expo-status-bar'
