b48abab3a33208de3823d2d0717310adc15f5111
v4v2 <tai@tilde.team>
2025-02-10 16:39:10 -0300

    tests(unit): validation of address, descriptors, etc

--- apps/mobile/utils/validation.ts
+++ apps/mobile/utils/validation.ts
@@ -1,8 +1,12 @@
+import ecc from '@bitcoinerlab/secp256k1'
 import * as bitcoinjs from 'bitcoinjs-lib'
 
+bitcoinjs.initEccLib(ecc)
+
 export function validateExtendedKey(key: string) {
-  // TODO: check string length
-  return key.match(new RegExp('^[xyz](pub|priv)[a-zA-Z0-9]+$')) !== null
+  // TODO: validate string length
+  // TODO: validate checksum
+  return key.match(new RegExp('^[xyz](pub|prv)[a-zA-Z0-9]+$')) !== null
 }
 
 export function validateDerivationPath(path: string) {
@@ -10,13 +14,13 @@ export function validateDerivationPath(path: string) {
 }
 
 export function validateFingerprint(fingerprint: string) {
-  return fingerprint.match(new RegExp('^[a-fA-F0-9]{6}$')) !== null
+  return fingerprint.match(new RegExp('^[a-fA-F0-9]{8}$')) !== null
 }
 
 export function validateDescriptor(descriptor: string) {
   // TODO: this validates simple descriptors, but not complex ones
   const r = new RegExp(
-    /(sh|wsh|pk|pkh|wpkh|combo|multi|sortedmulti|tr|addr|raw|rawtr)\((\[([a-fA-F0-9]{6})?(\/[0-9]+[h']?)+\])?[a-z0-9]+(\/[0-9*])*\)(#[a-z0-9]{8})?$/gm
+    /^(sh|wsh|pk|pkh|wpkh|combo|multi|sortedmulti|tr|addr|raw|rawtr)\((\[([a-fA-F0-9]{8})?(\/[0-9]+[h']?)+\])?[a-z0-9]+(\/[0-9*])*\)(#[a-z0-9]{8})?$/gim
   )
   return descriptor.match(r) !== null
 }
