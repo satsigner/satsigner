8f3eceaa132b067c8a3c003ecc34e0a86b0e4b85
Carlos Marques <psycarlo1@gmail.com>
2025-02-22 12:41:37 +0000

    fix: type import

--- apps/mobile/store/accounts.ts
+++ apps/mobile/store/accounts.ts
@@ -1,4 +1,4 @@
-import type { Descriptor, Wallet } from 'bdk-rn'
+import { type Descriptor, type Wallet } from 'bdk-rn'
 import { type Network } from 'bdk-rn/lib/lib/enums'
 import { produce } from 'immer'
 import { create } from 'zustand'
