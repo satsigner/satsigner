20d9bdd32c6d45ea817a016ce85e6fddc2b61e1e
Carlos Marques <psycarlo1@gmail.com>
2025-02-25 13:05:42 +0000

    fix: SSSelectModal to work with select policy type modal

--- apps/mobile/components/SSSelectModal.tsx
+++ apps/mobile/components/SSSelectModal.tsx
@@ -13,8 +13,8 @@ import SSText from './SSText'
 type SSSelectModalProps = {
   visible: boolean
   title: string
-  selectedText: string
-  selectedDescription: string | React.ReactNode
+  selectedText?: string
+  selectedDescription?: string | React.ReactNode
   onSelect(): void
   onCancel(): void
   children: React.ReactNode
@@ -29,7 +29,7 @@ function SSSelectModal({
   onCancel,
   children
 }: SSSelectModalProps) {
-  const splitSelectedText = selectedText.split(' - ')
+  const splitSelectedText = selectedText?.split(' - ') || []
 
   useEffect(() => {
     if (Platform.OS !== 'android') return
@@ -49,26 +49,28 @@ function SSSelectModal({
                 <SSText color="muted" size="lg" style={{ alignSelf: 'center' }}>
                   {title}
                 </SSText>
-                <SSVStack gap="sm">
-                  <SSText uppercase>
-                    {splitSelectedText.length > 1 ? (
-                      <>
-                        <SSText weight="bold">{splitSelectedText[0]}</SSText>
-                        {' - '}
-                        {splitSelectedText[1]}
-                      </>
+                {selectedText && selectedDescription && (
+                  <SSVStack gap="sm">
+                    <SSText uppercase>
+                      {splitSelectedText.length > 1 ? (
+                        <>
+                          <SSText weight="bold">{splitSelectedText[0]}</SSText>
+                          {' - '}
+                          {splitSelectedText[1]}
+                        </>
+                      ) : (
+                        selectedText
+                      )}
+                    </SSText>
+                    {typeof selectedDescription === 'string' ? (
+                      <SSText color="muted">{selectedDescription}</SSText>
                     ) : (
-                      selectedText
+                      selectedDescription
                     )}
-                  </SSText>
-                  {typeof selectedDescription === 'string' ? (
-                    <SSText color="muted">{selectedDescription}</SSText>
-                  ) : (
-                    selectedDescription
-                  )}
-                </SSVStack>
+                  </SSVStack>
+                )}
               </SSVStack>
-              <SSVStack>{children}</SSVStack>
+              <SSVStack style={{ paddingBottom: 16 }}>{children}</SSVStack>
             </SSVStack>
           </ScrollView>
           <SSVStack>
