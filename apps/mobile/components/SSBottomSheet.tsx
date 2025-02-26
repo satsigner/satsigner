adc83d01a04c1bfcd94f36d841c7c20a01915f46
Carlos Marques <psycarlo1@gmail.com>
2025-02-26 18:12:52 +0000

    feat: add SSBottomSheet

---
+++ apps/mobile/components/SSBottomSheet.tsx
@@ -0,0 +1,49 @@
+import BottomSheet, { BottomSheetScrollView } from '@gorhom/bottom-sheet'
+import type { BottomSheetMethods } from '@gorhom/bottom-sheet/lib/typescript/types'
+import { type ForwardedRef, forwardRef } from 'react'
+
+import SSVStack from '@/layouts/SSVStack'
+import { Colors, Layout } from '@/styles'
+
+import SSText from './SSText'
+
+// Note: It needs to be nested inside a GestureHandlerRootView to work
+
+type SSBottomSheetProps = {
+  title: string
+  snapPoints?: (string | number)[]
+  children: React.ReactNode
+}
+
+function SSBottomSheet(
+  { title, snapPoints = ['50%'], children }: SSBottomSheetProps,
+  ref: ForwardedRef<BottomSheetMethods>
+) {
+  return (
+    <BottomSheet
+      ref={ref}
+      index={-1}
+      snapPoints={snapPoints}
+      enablePanDownToClose
+      backgroundStyle={{ backgroundColor: Colors.gray[900] }}
+      handleStyle={{ display: 'none' }}
+    >
+      <BottomSheetScrollView
+        style={{
+          flex: 1,
+          paddingTop: 16,
+          paddingHorizontal: Layout.mainContainer.paddingHorizontal
+        }}
+      >
+        <SSVStack>
+          <SSText weight="bold" uppercase center>
+            {title}
+          </SSText>
+          {children}
+        </SSVStack>
+      </BottomSheetScrollView>
+    </BottomSheet>
+  )
+}
+
+export default forwardRef(SSBottomSheet)
