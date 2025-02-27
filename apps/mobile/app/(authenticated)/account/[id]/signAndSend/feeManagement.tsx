af0fb1cfb00580e3197d82f23986191af8c6c040
v4v2 <tai@tilde.team>
2025-02-27 16:58:07 -0300

    ui: add transaction RBF & CPFP options

---
+++ apps/mobile/app/(authenticated)/account/[id]/signAndSend/feeManagement.tsx
@@ -0,0 +1,72 @@
+import { router, Stack, useLocalSearchParams } from 'expo-router'
+import { useState } from 'react'
+import { useShallow } from 'zustand/react/shallow'
+
+import SSButton from '@/components/SSButton'
+import SSCheckbox from '@/components/SSCheckbox'
+import SSText from '@/components/SSText'
+import SSMainLayout from '@/layouts/SSMainLayout'
+import SSVStack from '@/layouts/SSVStack'
+import { t } from '@/locales'
+import { useTransactionBuilderStore } from '@/store/transactionBuilder'
+import { type AccountSearchParams } from '@/types/navigation/searchParams'
+
+function FeeManagement() {
+  const { id } = useLocalSearchParams<AccountSearchParams>()
+  const [rbf, cpfp, setRbf, setCpfp] = useTransactionBuilderStore(
+    useShallow((state) => [state.rbf, state.cpfp, state.setRbf, state.setCpfp])
+  )
+
+  const [localRBF, setLocalRBF] = useState(rbf)
+  const [localCPFP, setLocalCPFP] = useState(cpfp)
+
+  function cancel() {
+    router.navigate(`/account/${id}/signAndSend/ioPreview`)
+  }
+
+  function saveChanges() {
+    setRbf(localRBF)
+    setCpfp(localCPFP)
+    router.navigate(`/account/${id}/signAndSend/ioPreview`)
+  }
+
+  return (
+    <>
+      <Stack.Screen
+        options={{
+          headerTitle: () => <SSText uppercase>EXTRA SECURITY</SSText>
+        }}
+      />
+      <SSMainLayout style={{ paddingBottom: 24 }}>
+        <SSVStack justifyBetween>
+          <SSVStack>
+            <SSCheckbox
+              selected={localRBF}
+              label="Replace By Fee"
+              onPress={() => setLocalRBF(!localRBF)}
+            />
+            <SSCheckbox
+              selected={localCPFP}
+              label="Child Pays For Parents"
+              onPress={() => setLocalCPFP(!localCPFP)}
+            />
+          </SSVStack>
+          <SSVStack>
+            <SSButton
+              variant="secondary"
+              label={t('common.save')}
+              onPress={saveChanges}
+            />
+            <SSButton
+              variant="ghost"
+              label={t('common.cancel')}
+              onPress={cancel}
+            />
+          </SSVStack>
+        </SSVStack>
+      </SSMainLayout>
+    </>
+  )
+}
+
+export default FeeManagement
