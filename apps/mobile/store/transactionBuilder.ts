bfdbaab1c7de3075b21df5c0f40fa87f1b790087
Carlos Marques <psycarlo1@gmail.com>
2025-02-27 13:49:44 +0000

    feat: support for n:n

--- apps/mobile/store/transactionBuilder.ts
+++ apps/mobile/store/transactionBuilder.ts
@@ -14,6 +14,7 @@ type TransactionBuilderState = {
   inputs: Map<ReturnType<typeof getUtxoOutpoint>, Utxo>
   outputs: Output[]
   feeRate: number
+  rbf: boolean
   txBuilderResult?: TxBuilderResult
   psbt?: PartiallySignedTransaction
 }
@@ -25,9 +26,12 @@ type TransactionBuilderAction = {
   addInput: (utxo: Utxo) => void
   removeInput: (utxo: Utxo) => void
   addOutput: (output: Omit<Output, 'localId'>) => void
-  setFeeRate: (feeRate: number) => void
-  setTxBuilderResult: (txBuilderResult: TxBuilderResult) => void
-  setPsbt: (pbst: PartiallySignedTransaction) => void
+  setFeeRate: (feeRate: TransactionBuilderState['feeRate']) => void
+  setRbf: (rbf: TransactionBuilderState['rbf']) => void
+  setTxBuilderResult: (
+    txBuilderResult: NonNullable<TransactionBuilderState['txBuilderResult']>
+  ) => void
+  setPsbt: (pbst: NonNullable<TransactionBuilderState['psbt']>) => void
 }
 
 const useTransactionBuilderStore = create<
@@ -36,6 +40,7 @@ const useTransactionBuilderStore = create<
   inputs: new Map<ReturnType<typeof getUtxoOutpoint>, Utxo>(),
   outputs: [],
   feeRate: 0,
+  rbf: true,
   clearTransaction: () => {
     set({
       inputs: new Map<ReturnType<typeof getUtxoOutpoint>, Utxo>(),
@@ -74,6 +79,9 @@ const useTransactionBuilderStore = create<
   },
   setFeeRate: (feeRate) => {
     set({ feeRate })
+  },
+  setRbf: (rbf) => {
+    set({ rbf })
   },
   setTxBuilderResult: (txBuilderResult) => {
     set({ txBuilderResult })
