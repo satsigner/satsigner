a217339740c8cf03da5b9e05205eef79da1724ae
v4v2 <tai@tilde.team>
2025-02-28 10:02:33 -0300

    ui: transaction timelock screen improvements

--- apps/mobile/app/(authenticated)/account/[id]/signAndSend/timeLock.tsx
+++ apps/mobile/app/(authenticated)/account/[id]/signAndSend/timeLock.tsx
@@ -12,6 +12,15 @@ import SSVStack from '@/layouts/SSVStack'
 import { t } from '@/locales'
 import { type AccountSearchParams } from '@/types/navigation/searchParams'
 
+const CURRENT_BLOCK_HEIGHT = 885_000
+const AVERAGE_BLOCKS_PER_YEAR = 52560
+
+// the goal of the limit is to prevent users of making the mistake of getting
+// funds locked for more than 2 years
+const SAFE_TIMELOCK_LIMIT = CURRENT_BLOCK_HEIGHT + AVERAGE_BLOCKS_PER_YEAR * 2
+
+const DAYS_BY_MONTH = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+
 function TimeLock() {
   const { id } = useLocalSearchParams<AccountSearchParams>()
 
@@ -25,8 +34,14 @@ function TimeLock() {
   const [month, setMonth] = useState('')
   const [hour, setHour] = useState('')
   const [minute, setMinute] = useState('')
+  const [blockHeight, setBlockHeight] = useState('')
 
-  const daysByMonth = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+  const [validDay, setValidDay] = useState(false)
+  const [validYear, setValidYear] = useState(false)
+  const [validMonth, setValidMonth] = useState(false)
+  const [validHour, setValidHour] = useState(false)
+  const [validMinute, setValidMinute] = useState(false)
+  const [validBlockHeight, setValidBlockHeight] = useState(false)
 
   const renderTab = () => {
     return (
@@ -56,7 +71,17 @@ function TimeLock() {
   }: SceneRendererProps & { route: { key: string } }) => {
     switch (route.key) {
       case 'BLOCK HEIGHT':
-        return <SSText>BLOCK HEIGHT</SSText>
+        return (
+          <SSNumberInput
+            min={CURRENT_BLOCK_HEIGHT}
+            max={SAFE_TIMELOCK_LIMIT}
+            value={blockHeight}
+            onChangeText={setBlockHeight}
+            onValidate={setValidBlockHeight}
+            placeholder="BLOCK HEIGHT"
+            showFeedback
+          />
+        )
       case 'DATE':
         return (
           <SSVStack gap="lg">
@@ -68,6 +93,7 @@ function TimeLock() {
                   placeholder="YEAR"
                   value={year}
                   onChangeText={setYear}
+                  onValidate={setValidYear}
                 />
               </View>
               <View style={{ width: 'auto', flexGrow: 1 }}>
@@ -77,15 +103,17 @@ function TimeLock() {
                   placeholder="MONTH"
                   value={month}
                   onChangeText={setMonth}
+                  onValidate={setValidMonth}
                 />
               </View>
               <View style={{ width: 'auto', flexGrow: 1 }}>
                 <SSNumberInput
                   min={1}
-                  max={daysByMonth[Number(month) - 1 || 0]}
+                  max={DAYS_BY_MONTH[Number(month) - 1 || 0]}
                   placeholder="DAY"
                   value={day}
                   onChangeText={setDay}
+                  onValidate={setValidDay}
                 />
               </View>
             </SSHStack>
@@ -97,6 +125,7 @@ function TimeLock() {
                   placeholder="HOUR"
                   value={hour}
                   onChangeText={setHour}
+                  onValidate={setValidHour}
                 />
               </View>
               <View style={{ width: 'auto', flexGrow: 1 }}>
@@ -106,6 +135,7 @@ function TimeLock() {
                   placeholder="MINUTE"
                   value={minute}
                   onChangeText={setMinute}
+                  onValidate={setValidMinute}
                 />
               </View>
             </SSHStack>
@@ -137,29 +167,40 @@ function TimeLock() {
       />
       <SSMainLayout style={{ paddingBottom: 24, paddingTop: 12 }}>
         <SSVStack justifyBetween>
-          <SSText center uppercase size="lg">
-            TIMELOCK
-          </SSText>
-          <TabView
-            swipeEnabled={false}
-            navigationState={{ index: tabIndex, routes: tabs }}
-            renderScene={renderScene}
-            renderTabBar={renderTab}
-            onIndexChange={setTabIndex}
-          />
-        </SSVStack>
-        <SSVStack>
-          <SSButton label="REMOVE" onPress={removeTimeLock} />
-          <SSButton
-            variant="secondary"
-            label={t('common.save')}
-            onPress={saveChanges}
-          />
-          <SSButton
-            variant="ghost"
-            label={t('common.cancel')}
-            onPress={cancel}
-          />
+          <SSVStack style={{ height: 300 }}>
+            <SSText center uppercase size="lg">
+              TIMELOCK
+            </SSText>
+            <TabView
+              swipeEnabled={false}
+              navigationState={{ index: tabIndex, routes: tabs }}
+              renderScene={renderScene}
+              renderTabBar={renderTab}
+              onIndexChange={setTabIndex}
+            />
+          </SSVStack>
+          <SSVStack>
+            <SSButton label="REMOVE" onPress={removeTimeLock} />
+            <SSButton
+              variant="secondary"
+              label={t('common.save')}
+              onPress={saveChanges}
+              disabled={
+                (timeLockType === 'BLOCK HEIGHT' && !validBlockHeight) ||
+                (timeLockType === 'DATE' &&
+                  (!validYear ||
+                    !validMonth ||
+                    !validDay ||
+                    !validHour ||
+                    !validMinute))
+              }
+            />
+            <SSButton
+              variant="ghost"
+              label={t('common.cancel')}
+              onPress={cancel}
+            />
+          </SSVStack>
         </SSVStack>
       </SSMainLayout>
     </>
