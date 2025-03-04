e099e41adad7faa2245563aa4be2a0b8dd826191
NerdNook-rgb <alexismartin5510@gmail.com>
2025-03-04 02:32:36 -0800

    feat: implant drawer menu and coming soon page

---
+++ apps/mobile/components/SSNavMenu.tsx
@@ -0,0 +1,75 @@
+import { DrawerContentScrollView } from '@react-navigation/drawer'
+import { LinearGradient } from 'expo-linear-gradient'
+import { StyleSheet, View } from 'react-native'
+
+import { navMenuGroups } from '@/constants/navItems'
+import SSVStack from '@/layouts/SSVStack'
+import { t } from '@/locales'
+import { Colors } from '@/styles'
+
+import SSNavMenuGroup from './SSNavMenuGroup'
+import SSText from './SSText'
+
+function SSNavMenu(props: any) {
+  return (
+    <View style={styles.container}>
+      <LinearGradient
+        colors={[Colors.transparent, Colors.gray[900]]}
+        start={{ x: 0.8, y: 0 }}
+        end={{ x: 0.99, y: 0 }}
+        style={styles.gradientOverlay}
+        pointerEvents="none"
+      />
+      <SSText size="sm" color="muted" style={styles.versionText}>
+        {t('navigation.label.version')}
+      </SSText>
+      <DrawerContentScrollView
+        {...props}
+        contentContainerStyle={styles.contentContainer}
+      >
+        <SSVStack style={styles.vStackWrapper} justifyBetween>
+          {navMenuGroups.map((group, index) => (
+            <SSNavMenuGroup key={`${index} - ${group.title}`} group={group} />
+          ))}
+        </SSVStack>
+      </DrawerContentScrollView>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: Colors.black
+  },
+  gradientOverlay: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    height: '100%',
+    width: '100%',
+    zIndex: 99999,
+    shadowColor: Colors.black,
+    shadowOffset: { width: 2, height: 0 },
+    shadowOpacity: 0.25,
+    shadowRadius: 3,
+    elevation: 5
+  },
+  versionText: {
+    position: 'absolute',
+    letterSpacing: 2,
+    bottom: 30,
+    right: 50
+  },
+  contentContainer: {
+    flex: 1
+  },
+  vStackWrapper: {
+    padding: 12,
+    paddingRight: 22,
+    paddingTop: 40
+  }
+})
+
+export default SSNavMenu
---
+++ apps/mobile/components/icons/SSIconHamburger.tsx
@@ -0,0 +1,15 @@
+import Svg, { G, Path, type SvgProps } from 'react-native-svg'
+
+type IconProps = Pick<SvgProps, 'width' | 'height'>
+
+export default function SSIconHamburger({ width, height }: IconProps) {
+  return (
+    <Svg width={width} height={height} viewBox="0 0 14 10" fill="none">
+      <G id="hamburger" transform="translate(-2.596 -2.25)">
+        <Path d="M0 1H14" stroke="#A6A6A6" />
+        <Path d="M0 6H14" stroke="#A6A6A6" />
+        <Path d="M0 11H14" stroke="#A6A6A6" />
+      </G>
+    </Svg>
+  )
+}
