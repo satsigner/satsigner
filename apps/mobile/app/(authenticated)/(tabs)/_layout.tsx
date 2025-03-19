ae0d4ffb9269dc36e3ea1701ba0344119e4d8a8f
NerdNook-rgb <alexismartin5510@gmail.com>
2025-03-19 08:22:59 -0700

    fix: tab item different padding

--- apps/mobile/app/(authenticated)/(tabs)/_layout.tsx
+++ apps/mobile/app/(authenticated)/(tabs)/_layout.tsx
@@ -1,4 +1,5 @@
 import { Tabs } from 'expo-router'
+import { View, StyleSheet } from 'react-native'
 
 import {
   SSIconConverter,
@@ -8,62 +9,84 @@ import {
   SSIconSigner,
   SSIconSignerActive
 } from '@/components/icons'
+import { text } from '@/styles/sizes'
 
 export default function TabLayout() {
   return (
-    <>
-      <Tabs
-        screenOptions={{
-          headerShown: false,
-          tabBarStyle: {
-            backgroundColor: '#1F1F1F',
-            borderColor: '#323232',
-            paddingTop: 8,
-            paddingBottom: 8,
-            height: 64,
-            alignItems: 'center'
-          },
-          tabBarItemStyle: {
-            marginHorizontal: 16,
-            padding: 3,
-            borderRadius: 4,
-            maxWidth: 90
-          },
-          tabBarLabelStyle: {
-            textTransform: 'uppercase',
-            fontSize: 9,
-            letterSpacing: 1,
-            marginTop: 2
-          },
-          tabBarActiveBackgroundColor: 'black',
-          tabBarActiveTintColor: 'white'
+    <Tabs
+      screenOptions={{
+        headerShown: false,
+        tabBarStyle: styles.tabBar,
+        tabBarItemStyle: styles.tabBarItem,
+        tabBarLabelStyle: styles.tabBarLabel,
+        tabBarActiveBackgroundColor: 'black',
+        tabBarActiveTintColor: 'white'
+      }}
+    >
+      <Tabs.Screen
+        name="(signer)"
+        options={{
+          title: 'Signer',
+          tabBarIcon: ({ focused }) =>
+            renderTabIcon(focused, SSIconSignerActive, SSIconSigner)
         }}
-      >
-        <Tabs.Screen
-          name="(signer)"
-          options={{
-            title: 'Signer',
-            tabBarIcon: ({ focused }) =>
-              focused ? <SSIconSignerActive /> : <SSIconSigner />
-          }}
-        />
-        <Tabs.Screen
-          name="(explorer)"
-          options={{
-            title: 'Explorer',
-            tabBarIcon: ({ focused }) =>
-              focused ? <SSIconExplorerActive /> : <SSIconExplorer />
-          }}
-        />
-        <Tabs.Screen
-          name="(converter)"
-          options={{
-            title: 'Converter',
-            tabBarIcon: ({ focused }) =>
-              focused ? <SSIconConverterActive /> : <SSIconConverter />
-          }}
-        />
-      </Tabs>
-    </>
+      />
+      <Tabs.Screen
+        name="(explorer)"
+        options={{
+          title: 'Explorer',
+          tabBarIcon: ({ focused }) =>
+            renderTabIcon(focused, SSIconExplorerActive, SSIconExplorer)
+        }}
+      />
+      <Tabs.Screen
+        name="(converter)"
+        options={{
+          title: 'Converter',
+          tabBarIcon: ({ focused }) =>
+            renderTabIcon(focused, SSIconConverterActive, SSIconConverter)
+        }}
+      />
+    </Tabs>
   )
 }
+
+const renderTabIcon = (
+  focused: boolean,
+  ActiveIcon: React.ElementType,
+  InactiveIcon: React.ElementType
+) => (
+  <View style={styles.iconContainer}>
+    {focused ? <ActiveIcon /> : <InactiveIcon />}
+  </View>
+)
+
+const styles = StyleSheet.create({
+  tabBar: {
+    backgroundColor: '#1F1F1F',
+    borderColor: '#323232',
+    paddingTop: 8,
+    paddingBottom: 8,
+    height: 64,
+    alignItems: 'center'
+  },
+  tabBarItem: {
+    marginHorizontal: 16,
+    padding: 4,
+    borderRadius: 4,
+    maxWidth: 90
+  },
+  tabBarLabel: {
+    textTransform: 'uppercase',
+    fontSize: text.fontSize.xxs,
+    letterSpacing: 1,
+    marginTop: 2
+  },
+  iconContainer: {
+    width: 24,
+    height: 24,
+    marginTop: 4,
+    justifyContent: 'flex-start',
+    alignItems: 'center'
+  }
+})
