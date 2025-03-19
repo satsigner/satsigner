6d3b2d34e676dbb8431c2503591b8371cd081176
v4v2 <tai@tilde.team>
2025-03-19 15:05:54 -0300

    feat: add variants to display address into chunks

--- apps/mobile/components/SSAddressDisplay.tsx
+++ apps/mobile/components/SSAddressDisplay.tsx
@@ -8,12 +8,17 @@ import SSText, { type SSTextProps } from './SSText'
 
 type SSAddressDisplayProps = {
   address: string
+  variant?: 'box' | 'simple'
 } & SSTextProps
 
-function SSAddressDisplay({ address, ...props }: SSAddressDisplayProps) {
+function SSAddressDisplay({
+  address,
+  variant = 'box',
+  ...props
+}: SSAddressDisplayProps) {
   return (
     <SSClipboardCopy text={address}>
-      <SSHStack style={styles.box} gap="sm">
+      <SSHStack style={variant === 'box' ? styles.box : styles.simple} gap="sm">
         {(address.match(/(.{1,4})/g) || []).map((bytes, index) => (
           <SSText type="mono" size="md" {...props} key={index}>
             {bytes}
@@ -29,6 +34,10 @@ const styles = StyleSheet.create({
     backgroundColor: Colors.gray[800],
     flexWrap: 'wrap',
     padding: 12
+  },
+  simple: {
+    flexWrap: 'wrap',
+    padding: 0
   }
 })
 
