69e9a586fb5cb853819e231ecadcd5885d5b1722
v4v2 <tai@tilde.team>
2025-03-25 08:58:32 -0300

    chore: add sample script decoded fixture

---
+++ apps/mobile/components/SSScriptDecoded.fixture.tsx
@@ -0,0 +1,33 @@
+import { StyleSheet, View } from 'react-native'
+
+import { parseHexToBytes } from '@/utils/parse'
+
+import SSScriptDecoded from './SSScriptDecoded'
+import SSText from './SSText'
+
+function SSScriptDecodedFixture() {
+  const script = '76a91498b2553c30629340fac2a7729cf2f2100cdbb98d88ac'
+  return (
+    <View style={styles.container}>
+      <SSText size="xl" center uppercase>
+        Script Decoded
+      </SSText>
+      <SSScriptDecoded script={parseHexToBytes(script)} />
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingHorizontal: 20,
+    gap: 5,
+    justifyContent: 'center',
+    backgroundColor: '#000'
+  },
+  text: {
+    fontSize: 24
+  }
+})
+
+export default SSScriptDecodedFixture
