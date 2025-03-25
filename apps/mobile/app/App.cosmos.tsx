2c7bd59abff14eb1ca9db8c35570b79eb9818833
v4v2 <tai@tilde.team>
2025-03-25 14:37:10 -0300

    fix: typescript complaints

--- apps/mobile/app/App.cosmos.tsx
+++ apps/mobile/app/App.cosmos.tsx
@@ -1,6 +1,10 @@
 import { Component } from 'react'
 import { NativeFixtureLoader } from 'react-cosmos-native'
 
+// INFO: the file cosmos.imports is not tracked by git and used only in local
+// development, therefore we need to disable typescript checking this line.
+//
+// @ts-ignore:next-line
 import { moduleWrappers, rendererConfig } from '../cosmos.imports'
 
 export default class CosmosApp extends Component {
