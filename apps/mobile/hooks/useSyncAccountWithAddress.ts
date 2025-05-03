404af60706b96a85d767c0cc7392b475a6786e61
v4v2 <tai@tilde.team>
2025-05-03 16:25:58 -0300

    refact: esplora backend useSyncAccountWithAddress hook

--- apps/mobile/hooks/useSyncAccountWithAddress.ts
+++ apps/mobile/hooks/useSyncAccountWithAddress.ts
@@ -8,14 +8,23 @@ import { useAccountsStore } from '@/store/accounts'
 import { useBlockchainStore } from '@/store/blockchain'
 import { type Account } from '@/types/models/Account'
 import { type Transaction } from '@/types/models/Transaction'
+import { type Utxo } from '@/types/models/Utxo'
 import { formatTimestamp } from '@/utils/format'
 import { parseAddressDescriptorToAddress, parseHexToBytes } from '@/utils/parse'
 import { getUtxoOutpoint } from '@/utils/utxo'
 
+type AddressInfo = {
+  transactions: Transaction[]
+  utxos: Utxo[]
+  confirmed: number
+  unconfirmed: number
+  progress: Account['syncProgress']
+}
+
 // Hook required because bdk does not support address descriptor
 function useSyncAccountWithAddress() {
-  const [setSyncStatus, setSyncProgress] = useAccountsStore(
-    useShallow((state) => [state.setSyncStatus, state.setSyncProgress])
+  const [setSyncStatus, updateAccount] = useAccountsStore(
+    useShallow((state) => [state.setSyncStatus, state.updateAccount])
   )
 
   const [backend, network, url] = useBlockchainStore(
@@ -31,21 +40,50 @@ function useSyncAccountWithAddress() {
     account: Account,
     address: string,
     url: string
-  ) {
+  ): Promise<AddressInfo> {
     const transactions: Account['transactions'] = []
-    let utxos: Account['utxos'] = []
+    const utxos: Account['utxos'] = []
     let confirmed = 0
     let unconfirmed = 0
 
     const esploraClient = new Esplora(url)
 
-    const esploraTxs = await esploraClient.getAddressTx(address) // 1
-    const esploraUtxos = await esploraClient.getAddressUtxos(address) // 2
+    // update sync progress
+    account.syncProgress = {
+      totalTasks: account.syncProgress?.totalTasks || 0,
+      tasksDone: account.syncProgress?.tasksDone || 0
+    }
+    account.syncProgress.totalTasks += 2
+    updateAccount(account)
+
+    // make the request
+    const esploraTxs = await esploraClient.getAddressTx(address)
+    const esploraUtxos = await esploraClient.getAddressUtxos(address)
+
+    // compute how much more requests are needed
+    const existingTx: Record<string, number> = {}
+    for (let txIndex = 0; txIndex < account.transactions.length; txIndex++) {
+      const tx = account.transactions[txIndex]
+      existingTx[tx.id] = txIndex as number
+    }
+    for (const tx of esploraTxs) {
+      if (existingTx[tx.txid] !== undefined) {
+        account.syncProgress.totalTasks += 1
+      }
+    }
+    updateAccount(account)
 
     const txDictionary: Record<string, number> = {}
 
     for (let index = 0; index < esploraTxs.length; index++) {
       const t = esploraTxs[index]
+
+      if (existingTx[t.txid] !== undefined) {
+        const txIndex = existingTx[t.txid]
+        transactions.push(account.transactions[txIndex])
+        continue
+      }
+
       const vin: Transaction['vin'] = []
       const vout: Transaction['vout'] = []
       let sent = 0
@@ -77,7 +115,7 @@ function useSyncAccountWithAddress() {
         }
       })
 
-      const raw = await esploraClient.getTxHex(t.txid) // t
+      const raw = await esploraClient.getTxHex(t.txid)
 
       const tx = {
         address,
@@ -104,32 +142,50 @@ function useSyncAccountWithAddress() {
       transactions.push(tx)
     }
 
-    utxos = esploraUtxos.map((u) => {
-      if (u.status.confirmed) confirmed += u.value
-      else unconfirmed += u.value
+    // update utxos
+    utxos.push(
+      ...esploraUtxos.map((u) => {
+        if (u.status.confirmed) {
+          confirmed += u.value
+        } else {
+          unconfirmed += u.value
+        }
 
-      let script: number[] | undefined
-      if (txDictionary[u.txid] !== undefined) {
-        const index = txDictionary[u.txid]
-        const tx = esploraTxs[index]
-        script = parseHexToBytes(tx.vout[u.vout].scriptpubkey)
-      }
+        let script: number[] | undefined
 
-      return {
-        txid: u.txid,
-        vout: u.vout,
-        value: u.value,
-        label: '',
-        addressTo: address,
-        keychain: 'external',
-        script,
-        timestamp: u.status.block_time
-          ? new Date(u.status.block_time * 1000)
-          : undefined
-      }
-    })
+        if (txDictionary[u.txid] !== undefined) {
+          const index = txDictionary[u.txid]
+          const tx = esploraTxs[index]
+          script = parseHexToBytes(tx.vout[u.vout].scriptpubkey)
+        }
 
-    // return { transactions, utxos, confirmed, unconfirmed }
+        return {
+          txid: u.txid,
+          vout: u.vout,
+          value: u.value,
+          label: '',
+          addressTo: address,
+          keychain: 'external',
+          script,
+          timestamp: u.status.block_time
+            ? new Date(u.status.block_time * 1000)
+            : undefined
+        } as Utxo
+      })
+    )
+
+    // update account
+    account.transactions = transactions
+    account.utxos = utxos
+    updateAccount(account)
+
+    return {
+      transactions,
+      utxos,
+      confirmed,
+      unconfirmed,
+      progress: account.syncProgress
+    }
   }
 
   async function syncAccountWithAddressUsingElectrum(
@@ -137,8 +193,19 @@ function useSyncAccountWithAddress() {
     address: string,
     url: string,
     network: Network
-  ) {
-    //
+  ): Promise<AddressInfo> {
+    const electrumClient = ElectrumClient.fromUrl(url, network)
+    await electrumClient.init()
+    const addrInfo = await electrumClient.getAddressInfo(address)
+    try {
+      electrumClient.close()
+    } catch {
+      //
+    }
+    transactions = addrInfo.transactions
+    utxos = addrInfo.utxos
+    confirmed = addrInfo.balance.confirmed
+    unconfirmed = addrInfo.balance.unconfirmed
   }
 
   async function syncAccountWithAddress(
@@ -158,45 +225,48 @@ function useSyncAccountWithAddress() {
         labelsBackup[getUtxoOutpoint(utxo)] = utxo.label || ''
       }
 
+      const updatedAccount: Account = { ...account }
       const address = parseAddressDescriptorToAddress(addressDescriptor)
 
-      const updatedAccount: Account = { ...account }
+      // update account sync progress
+      updatedAccount.syncProgress = {
+        tasksDone: 0,
+        totalTasks: 0
+      }
+      updateAccount(updatedAccount)
 
-      let transactions: Account['transactions'] = []
-      let utxos: Account['utxos'] = []
-      let confirmed = 0
-      let unconfirmed = 0
+      let addrInfo: AddressInfo | undefined
 
       if (backend === 'esplora') {
+        addrInfo = await syncAccountWithAddressUsingEsplora(
+          updatedAccount,
+          address,
+          url
+        )
       } else if (backend === 'electrum') {
-        const electrumClient = ElectrumClient.fromUrl(url, network)
-
-        await electrumClient.init()
-        const addrInfo = await electrumClient.getAddressInfo(address)
-        try {
-          electrumClient.close()
-        } catch {
-          //
-        }
-        transactions = addrInfo.transactions
-        utxos = addrInfo.utxos
-        confirmed = addrInfo.balance.confirmed
-        unconfirmed = addrInfo.balance.unconfirmed
+        addrInfo = await syncAccountWithAddressUsingElectrum(
+          updatedAccount,
+          address,
+          url,
+          network
+        )
+      } else {
+        throw new Error('unkown backend')
       }
 
       const summary = {
         numberOfAddresses: 1,
-        numberOfTransactions: transactions.length,
-        numberOfUtxos: utxos.length,
-        satsInMempool: unconfirmed,
-        balance: confirmed
+        numberOfTransactions: addrInfo.transactions.length,
+        numberOfUtxos: addrInfo.utxos.length,
+        satsInMempool: addrInfo.unconfirmed,
+        balance: addrInfo.confirmed
       }
 
-      updatedAccount.transactions = transactions
-      updatedAccount.utxos = utxos
+      updatedAccount.transactions = addrInfo.transactions
+      updatedAccount.utxos = addrInfo.utxos
       updatedAccount.summary = summary
 
-      //Labels update
+      // Labels update
       for (const index in updatedAccount.utxos) {
         const utxoRef = getUtxoOutpoint(updatedAccount.utxos[index])
         updatedAccount.utxos[index].label = labelsBackup[utxoRef] || ''
@@ -207,20 +277,39 @@ function useSyncAccountWithAddress() {
           labelsBackup[transactionRef] || ''
       }
 
-      // Extract timestamps
-      const timestamps = updatedAccount.transactions
-        .filter((transaction) => transaction.timestamp)
-        .map((transaction) => formatTimestamp(transaction.timestamp!))
+      // timestamps of transactions without price data
+      const timestamps = [
+        ...new Set(
+          updatedAccount.transactions
+            .filter((transaction) => {
+              return transaction.timestamp && transaction.prices['USD']
+            })
+            .map((transaction) => {
+              return formatTimestamp(transaction.timestamp!)
+            })
+        )
+      ]
 
-      //Fetch Prices
+      // Update progress status
+      const previousTasksDone = updatedAccount.syncProgress?.tasksDone || 0
+      const previousTotalTasks = updatedAccount.syncProgress?.totalTasks || 0
+      updatedAccount.syncProgress = {
+        tasksDone: previousTasksDone,
+        totalTasks: previousTotalTasks + timestamps.length
+      }
+
+      // fetch prices
       const oracle = new MempoolOracle()
       const prices = await oracle.getPricesAt('USD', timestamps)
-
-      //Transaction prices update
       for (const index in updatedAccount.transactions) {
         updatedAccount.transactions[index].prices = { USD: prices[index] }
       }
 
+      // Update account progress again
+      updatedAccount.syncProgress.tasksDone += timestamps.length
+      updateAccount(updatedAccount)
+
+      // Update sync status
       updatedAccount.syncStatus = 'synced'
       updatedAccount.lastSyncedAt = new Date()
 
