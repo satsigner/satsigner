dc44663bdbabe2ea9fc1ae100ae972a52a2de0f4
NerdNook-rgb <henrywinslow9519@gmail.com>
2025-05-06 14:49:36 -0400

    feature: parse multiple outputs

--- apps/mobile/app/(authenticated)/(tabs)/(signer,explorer,converter)/account/[id]/signAndSend/importOutputs.tsx
+++ apps/mobile/app/(authenticated)/(tabs)/(signer,explorer,converter)/account/[id]/signAndSend/importOutputs.tsx
@@ -2,6 +2,7 @@ import * as Clipboard from 'expo-clipboard'
 import { Stack, useRouter } from 'expo-router'
 import { useState } from 'react'
 import { ScrollView, View } from 'react-native'
+import { useShallow } from 'zustand/react/shallow'
 
 import SSButton from '@/components/SSButton'
 import SSText from '@/components/SSText'
@@ -9,7 +10,9 @@ import SSHStack from '@/layouts/SSHStack'
 import SSMainLayout from '@/layouts/SSMainLayout'
 import SSVStack from '@/layouts/SSVStack'
 import { t, tn as _tn } from '@/locales'
+import { useTransactionBuilderStore } from '@/store/transactionBuilder'
 import { Colors } from '@/styles'
+import { parseTXOutputs } from '@/utils/parse'
 
 const tn = _tn('transaction.build.options.importOutputs')
 
@@ -18,6 +21,10 @@ function ImportOuputs() {
   const [importedOutputs, setImportedOutputs] = useState(tn('emptyContent'))
   const [validInput, setValidInput] = useState(false)
 
+  const [addOutput] = useTransactionBuilderStore(
+    useShallow((state) => [state.addOutput])
+  )
+
   async function readFromClibpoard() {
     const text = await Clipboard.getStringAsync()
     if (text) setImportedOutputs(text)
@@ -25,6 +32,11 @@ function ImportOuputs() {
   }
 
   function importOutputs() {
+    const parsedOutputs = parseTXOutputs(importedOutputs)
+
+    parsedOutputs.map((output) => {
+      addOutput(output)
+    })
     router.back()
   }
 
