9f34e47bd717c9d9d3070d72b72ce64f1430b071
v4v2 <tai@tilde.team>
2025-06-05 17:09:13 -0300

    feat: add hook to correctly get the number of used addresses

---
+++ apps/mobile/hooks/useGetNumberOfUsedAddresses.ts
@@ -0,0 +1,49 @@
+import { type Wallet } from 'bdk-rn/lib/classes/Wallet'
+import { useEffect, useState } from 'react'
+
+import { useBlockchainStore } from '@/store/blockchain'
+import { type Account } from '@/types/models/Account'
+
+function useGetNumberOfUsedAddresses(wallet: Wallet, account: Account) {
+  const [addressCount, setAddressCount] = useState(0)
+
+  const stopGap = useBlockchainStore(
+    (state) => state.configs[account.network].config.stopGap
+  )
+
+  async function updateAddressCount() {
+    const seenAddresses: Record<string, boolean> = {}
+
+    for (const tx of account.transactions) {
+      for (const output of tx.vout) {
+        if (output.address) {
+          seenAddresses[output.address] = true
+        }
+      }
+    }
+
+    let index = 0
+    let lastIndexWithFunds = -1
+    let localAddressCount = 0
+
+    while (index > lastIndexWithFunds + stopGap) {
+      const addrInfo = await wallet.getAddress(index)
+      const addr = await addrInfo.address.asString()
+      if (seenAddresses[addr] !== undefined) {
+        lastIndexWithFunds = index
+        localAddressCount += 1
+      }
+      index += 1
+    }
+
+    setAddressCount(localAddressCount)
+  }
+
+  useEffect(() => {
+    updateAddressCount()
+  }, [account, wallet]) // eslint-disable-line react-hooks/exhaustive-deps
+
+  return addressCount
+}
+
+export default useGetNumberOfUsedAddresses
