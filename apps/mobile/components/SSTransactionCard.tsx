04768af40c3b3356c2244b0660d39ceefbb7afb7
v4v2 <tai@tilde.team>
2025-07-13 23:44:47 -0300

    fix: eslint error unused variable

--- apps/mobile/components/SSTransactionCard.tsx
+++ apps/mobile/components/SSTransactionCard.tsx
@@ -1,5 +1,5 @@
 import { useRouter } from 'expo-router'
-import { useEffect, useMemo, useState } from 'react'
+import { useEffect, useState } from 'react'
 import {
   type StyleProp,
   StyleSheet,
