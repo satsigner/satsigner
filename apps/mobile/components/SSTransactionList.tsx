589e9bb60171a22f48c905c8335d1a7d5f9df5ba
v4v2 <tai@tilde.team>
2025-07-28 19:22:35 -0300

    refact: add transaction list component which only shows transaction list

---
+++ apps/mobile/components/SSTransactionList.tsx
@@ -0,0 +1,108 @@
+import { FlashList } from '@shopify/flash-list'
+import { useMemo } from 'react'
+import { RefreshControl } from 'react-native'
+import { useShallow } from 'zustand/react/shallow'
+
+import SSBalanceChangeBar from '@/components/SSBalanceChangeBar'
+import SSTransactionCard from '@/components/SSTransactionCard'
+import SSVStack from '@/layouts/SSVStack'
+import { usePriceStore } from '@/store/price'
+import { Colors } from '@/styles'
+import { type Account } from '@/types/models/Account'
+import { type Transaction } from '@/types/models/Transaction'
+
+import SSText from './SSText'
+
+type SSTransactionListItem = {
+  accountId: Account['id']
+} & Transaction
+
+type SSTransactionListProps = {
+  transactions: SSTransactionListItem[]
+  handleOnRefresh: () => Promise<void>
+  expand: boolean
+  refreshing: boolean
+  blockchainHeight: number
+}
+
+function SSTransactionList({
+  transactions,
+  handleOnRefresh,
+  expand,
+  refreshing,
+  blockchainHeight
+}: SSTransactionListProps) {
+  const [btcPrice, fiatCurrency] = usePriceStore(
+    useShallow((state) => [state.btcPrice, state.fiatCurrency])
+  )
+
+  const transactionBalances = useMemo(() => {
+    let balance = 0
+    const balances = transactions.map((tx) => {
+      const received = tx.received || 0
+      const sent = tx.sent || 0
+      balance = balance + received - sent
+      return balance
+    })
+
+    return balances.reverse()
+  }, [transactions])
+
+  const maxBalance = useMemo(() => {
+    if (transactionBalances.length === 0) return 0
+    return Math.max(...transactionBalances)
+  }, [transactionBalances])
+
+  return (
+    <SSVStack
+      style={{
+        flex: 1,
+        marginLeft: 16,
+        marginRight: 2,
+        paddingRight: 14,
+        height: 400,
+        minHeight: 200
+      }}
+      gap={expand ? 'sm' : 'md'}
+    >
+      <FlashList
+        data={transactions.slice().reverse()}
+        renderItem={({ item, index }) => (
+          <SSVStack gap="none">
+            <SSBalanceChangeBar
+              transaction={item}
+              balance={transactionBalances[index]}
+              maxBalance={maxBalance}
+            />
+            <SSTransactionCard
+              btcPrice={btcPrice}
+              fiatCurrency={fiatCurrency}
+              transaction={item}
+              expand={expand}
+              walletBalance={transactionBalances[index]}
+              blockHeight={blockchainHeight}
+              link={`/account/${item.accountId}/transaction/${item.id}`}
+            />
+          </SSVStack>
+        )}
+        estimatedItemSize={120}
+        ListEmptyComponent={
+          <SSVStack>
+            <SSText>No transactions</SSText>
+          </SSVStack>
+        }
+        keyExtractor={(item) => item.id}
+        refreshControl={
+          <RefreshControl
+            refreshing={refreshing}
+            onRefresh={handleOnRefresh}
+            colors={[Colors.gray[950]]}
+            progressBackgroundColor={Colors.white}
+          />
+        }
+      />
+    </SSVStack>
+  )
+}
+
+export default SSTransactionList
