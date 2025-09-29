0348f7d46cbbfbc603605a8a76f71a025b970aae
Yi-Jacob <jacobyideveloper@gmail.com>
2025-09-29 00:19:03 -0400

    fix: import type of NostrDM

--- apps/mobile/store/accountBuilder.ts
+++ apps/mobile/store/accountBuilder.ts
@@ -6,8 +6,8 @@ import { PIN_KEY } from '@/config/auth'
 import { getItem } from '@/storage/encrypted'
 import { type EntropyType } from '@/types/logic/entropy'
 import { type Account, type Key, type Secret } from '@/types/models/Account'
+import { type NostrDM } from '@/types/models/Nostr'
 import { aesDecrypt, aesEncrypt } from '@/utils/crypto'
-import type { NostrDM } from '@/types/models/Nostr'
 
 type AccountBuilderState = {
   name: Account['name']
