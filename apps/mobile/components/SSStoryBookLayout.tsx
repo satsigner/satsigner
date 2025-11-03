ee7f800dcc0f300f250e23ef030fb69757d7a011
v4v2 <tai@tilde.team>
2025-11-03 14:57:52 -0300

    refact: add storybook layout to be reused across stories

---
+++ apps/mobile/components/SSStoryBookLayout.tsx
@@ -0,0 +1,30 @@
+import type { Decorator, StoryFn } from '@storybook/react'
+import { View } from 'react-native'
+
+import { Layout } from '@/styles'
+
+function SSStoryBookLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <View
+      style={{
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        paddingHorizontal: Layout.mainContainer.paddingHorizontal
+      }}
+    >
+      {children}
+    </View>
+  )
+}
+
+// It is not possible to export a function declaration because the decorators
+// of stories must be object instances (and not class or functions).
+// Thus, we actually are obligated to create an instance variable.
+const storybookLayoutDecorator: Decorator = (Story: StoryFn) => (
+  <SSStoryBookLayout>
+    <Story />
+  </SSStoryBookLayout>
+)
+
+export default storybookLayoutDecorator
