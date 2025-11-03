9f1d707b14d0e016f3875f336daa7e744578ec79
v4v2 <tai@tilde.team>
2025-11-03 17:06:31 -0300

    feat: show preview of result in label conflict solver WIP

--- apps/mobile/components/SSLabelConflict.tsx
+++ apps/mobile/components/SSLabelConflict.tsx
@@ -1,6 +1,9 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import { StyleSheet } from 'react-native'
 
+import SSHStack from '@/layouts/SSHStack'
 import SSVStack from '@/layouts/SSVStack'
+import { Colors } from '@/styles'
 import { type Label } from '@/utils/bip329'
 
 import SSCheckbox from './SSCheckbox'
@@ -18,6 +21,39 @@ type ConflictStrategy = (typeof conflictStrategies)[number]
 function SSLabelConflict({ conflicts }: SSLabelConflictProps) {
   const [conflictStrategy, setConflictStrategy] =
     useState<ConflictStrategy>('incoming')
+  const [result, setResult] = useState<Label[]>([])
+
+  useEffect(() => {
+    let results: Label[] = []
+
+    if (conflictStrategy === 'current') {
+      results = conflicts.map(([current, _incoming]) => {
+        return { ...current }
+      })
+    }
+
+    if (conflictStrategy === 'incoming') {
+      results = conflicts.map(([_current, incoming]) => {
+        return { ...incoming }
+      })
+    }
+
+    if (conflictStrategy === 'merge') {
+      results = conflicts.map(([current, incoming]) => {
+        return {
+          ...current,
+          ...incoming,
+          label: `${current.label}; ${incoming.label}`
+        }
+      })
+    }
+
+    if (conflictStrategy === 'manual') {
+      results = []
+    }
+
+    setResult(results)
+  }, [conflictStrategy, conflicts])
 
   return (
     <SSVStack>
@@ -36,23 +72,76 @@ function SSLabelConflict({ conflicts }: SSLabelConflictProps) {
       </SSVStack>
       {conflicts.map(([current, incoming], index) => {
         return (
-          <SSVStack key={index} gap="none">
+          <SSVStack key={index} gap="xs">
             <SSText uppercase weight="bold" size="md">
               Conflict {index + 1}
             </SSText>
+            <SSHStack gap="none">
+              <SSText weight="bold">Object type: </SSText>
+              <SSText>{current.type}</SSText>
+            </SSHStack>
             <SSVStack gap="none">
-              <SSText weight="bold">Current:</SSText>
-              <SSText size="md">{current.label}</SSText>
+              <SSText weight="bold">Object ref:</SSText>
+              <SSText type="mono">{current.ref}</SSText>
             </SSVStack>
             <SSVStack gap="none">
-              <SSText weight="bold">Incoming:</SSText>
-              <SSText size="md">{incoming.label}</SSText>
+              <SSText weight="bold">Current label:</SSText>
+              <SSText
+                size="md"
+                style={
+                  conflictStrategy === 'incoming'
+                    ? styles.rejected
+                    : conflictStrategy === 'current' ||
+                        conflictStrategy === 'merge'
+                      ? styles.accepted
+                      : styles.none
+                }
+              >
+                {current.label}
+              </SSText>
             </SSVStack>
+            <SSVStack gap="none">
+              <SSText weight="bold">Incoming label:</SSText>
+              <SSText
+                size="md"
+                style={
+                  conflictStrategy === 'current'
+                    ? styles.rejected
+                    : conflictStrategy === 'incoming' ||
+                        conflictStrategy === 'merge'
+                      ? styles.accepted
+                      : styles.none
+                }
+              >
+                {incoming.label}
+              </SSText>
+            </SSVStack>
+            {result[index] && (
+              <SSVStack gap="none">
+                <SSText weight="bold">Result:</SSText>
+                <SSText size="md" style={styles.info}>
+                  {result[index].label}
+                </SSText>
+              </SSVStack>
+            )}
           </SSVStack>
         )
       })}
     </SSVStack>
   )
 }
+
+const styles = StyleSheet.create({
+  accepted: {
+    backgroundColor: Colors.success
+  },
+  rejected: {
+    backgroundColor: Colors.error
+  },
+  none: {},
+  info: {
+    backgroundColor: Colors.info
+  }
+})
 
 export default SSLabelConflict
