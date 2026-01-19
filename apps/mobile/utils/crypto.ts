9b197989b74c113cfc5d5f92b6b51485f65cb19d
v4v2 <tai@tilde.team>
2026-01-18 22:55:36 -0300

    fix: implement Math.random() using crypto.getRandomValues() from polyfill
    The polyfill is already installed and it is react-native-get-random-values

--- apps/mobile/utils/crypto.ts
+++ apps/mobile/utils/crypto.ts
@@ -1,4 +1,4 @@
-import crypto from 'react-native-aes-crypto'
+import aesCrypto from 'react-native-aes-crypto'
 
 import { DEFAULT_PIN, PIN_KEY } from '@/config/auth'
 import { getItem } from '@/storage/encrypted'
@@ -6,47 +6,44 @@ import { getItem } from '@/storage/encrypted'
 const MAX_UINT32 = 0xffffffff // 2^32 - 1
 
 function randomKey(length = 16) {
-  return crypto.randomKey(length)
+  return aesCrypto.randomKey(length)
 }
 
 function randomUuid() {
-  return crypto.randomUuid()
+  return aesCrypto.randomUuid()
 }
 
-async function randomNum() {
-  const randomHex = await crypto.randomKey(32)
-  const random32BitsHex = randomHex.slice(0, 8)
-  const randomNumber = parseInt(random32BitsHex, 16)
-  const randomDecimal = randomNumber / (MAX_UINT32 + 1)
-  return randomDecimal
+function randomNum() {
+  // global variable from react-native-get-random-values
+  return crypto.getRandomValues(new Uint32Array(1))[0] / MAX_UINT32;
 }
 
 function sha256(text: string) {
-  return crypto.sha256(text)
+  return aesCrypto.sha256(text)
 }
 
 function aesEncrypt(text: string, key: string, iv: string) {
-  return crypto.encrypt(text, key, iv, 'aes-256-cbc')
+  return aesCrypto.encrypt(text, key, iv, 'aes-256-cbc')
 }
 
 function aesDecrypt(cipher: string, key: string, iv: string) {
-  return crypto.decrypt(cipher, key, iv, 'aes-256-cbc')
+  return aesCrypto.decrypt(cipher, key, iv, 'aes-256-cbc')
 }
 
 /** Password-based key derivation */
 function pbkdf2Encrypt(pin: string, salt: string) {
-  return crypto.pbkdf2(pin, salt, 10_000, 256, 'sha256')
+  return aesCrypto.pbkdf2(pin, salt, 10_000, 256, 'sha256')
 }
 
 function generateSalt() {
-  return crypto.randomKey(16)
+  return aesCrypto.randomKey(16)
 }
 
 async function doubleShaEncrypt(text: string) {
   // TODO: remove
-  const first = await crypto.sha256(text)
+  const first = await aesCrypto.sha256(text)
 
-  return crypto.sha256(first)
+  return aesCrypto.sha256(first)
 }
 
 // FIX: me
