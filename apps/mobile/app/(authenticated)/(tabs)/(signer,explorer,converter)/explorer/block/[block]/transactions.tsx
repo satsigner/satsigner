69ee99eb2ce241c39151e7863c2217e51831d627
v4v2 <tai@tilde.team>
2026-02-01 23:16:27 -0300

    ui: block transaction in explorer»block

--- apps/mobile/app/(authenticated)/(tabs)/(signer,explorer,converter)/explorer/block/[block]/transactions.tsx
+++ apps/mobile/app/(authenticated)/(tabs)/(signer,explorer,converter)/explorer/block/[block]/transactions.tsx
@@ -3,21 +3,34 @@ import { useLocalSearchParams } from 'expo-router'
 import { useEffect, useState } from 'react'
 import { ScrollView } from 'react-native'
 import { TouchableOpacity } from 'react-native-gesture-handler'
+import { toast } from 'sonner-native'
 
 import SSButton from '@/components/SSButton'
+import SSStyledSatText from '@/components/SSStyledSatText'
 import SSText from '@/components/SSText'
-import SSTransactionDecoded from '@/components/SSTransactionDecoded'
 import useMempoolOracle from '@/hooks/useMempoolOracle'
+import SSHStack from '@/layouts/SSHStack'
 import SSMainLayout from '@/layouts/SSMainLayout'
 import SSVStack from '@/layouts/SSVStack'
 import { type ExplorerBlockSearchParams } from '@/types/navigation/searchParams'
+
+type BlockTransaction = {
+  id: string
+  // version: number
+  // locktime: number
+  // hex: string
+  // weight: number
+  // vsize: number
+  // inputs: bitcoinjs.TxInput[]
+  // outputs: bitcoinjs.TxOutput[]
+  value: number
+}
 
 export default function BlockTransactions() {
   const { block: blockHash } = useLocalSearchParams<ExplorerBlockSearchParams>()
   const mempoolOracle = useMempoolOracle()
 
-  const [transactions, setTransactions] = useState<bitcoinjs.Transaction[]>([])
-  const [focusedTx, setFocusedTx] = useState(-1)
+  const [transactions, setTransactions] = useState<BlockTransaction[]>([])
   const [loading, setLoading] = useState(false)
   // const [block, setBlock] = useState<bitcoinjs.Block | null>(null)
 
@@ -25,7 +38,25 @@ export default function BlockTransactions() {
     setLoading(true)
     const blockRaw = await mempoolOracle.getBlockRaw(blockHash)
     const block = bitcoinjs.Block.fromBuffer(Buffer.from(blockRaw))
-    setTransactions(block.transactions || [])
+    if (!block.transactions) {
+      toast.error('Did not find transactions in this block')
+      return
+    }
+    setTransactions(
+      block.transactions.map((tx) => {
+        return {
+          id: tx.getId(),
+          // version: tx.version,
+          // locktime: tx.locktime,
+          // hex: tx.toHex(),
+          // weight: tx.weight(),
+          // vsize: tx.virtualSize(),
+          // inputs: tx.ins,
+          // outputs: tx.outs,
+          value: tx.outs.reduce((sum, out) => sum + out.value, 0)
+        }
+      })
+    )
     setLoading(false)
   }
 
@@ -43,17 +74,15 @@ export default function BlockTransactions() {
           onPress={fetchBlockTransactions}
         />
         <SSVStack gap="md">
-          {transactions.map((tx, index) => {
+          {transactions.map((tx) => {
             return (
-              <TouchableOpacity
-                key={tx.getId()}
-                onPress={() => setFocusedTx(index)}
-              >
-                <SSVStack>
-                  <SSText type="mono">{tx.getId()}</SSText>
-                  {focusedTx === index && (
-                    <SSTransactionDecoded txHex={tx.toHex()} />
-                  )}
+              <TouchableOpacity key={tx.id}>
+                <SSVStack gap="none">
+                  <SSText type="mono">{tx.id}</SSText>
+                  <SSHStack>
+                    <SSText>value:</SSText>
+                    <SSStyledSatText amount={tx.id} textSize="sm" />
+                  </SSHStack>
                 </SSVStack>
               </TouchableOpacity>
             )
