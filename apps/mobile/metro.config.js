a80af38deea10989b6cfac8107f9dea8fdcc2e34
psycarlo <psycarlo1@gmail.com>
2026-04-06 22:40:30 +0100

    chore: remove rn-nodeify

--- apps/mobile/metro.config.js
+++ apps/mobile/metro.config.js
@@ -2,6 +2,13 @@ const { getDefaultConfig } = require('expo/metro-config')
 
 const defaultConfig = getDefaultConfig(__dirname)
 
+defaultConfig.resolver.extraNodeModules = {
+  assert: require.resolve('assert/'),
+  buffer: require.resolve('buffer/'),
+  events: require.resolve('events/'),
+  process: require.resolve('process/'),
+}
+
 const withStorybook = require('@storybook/react-native/metro/withStorybook')
 
 module.exports = withStorybook(defaultConfig)
