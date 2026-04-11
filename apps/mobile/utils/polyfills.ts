ab388d6e45c57bf00ba3cb2bbe01c50032853df3
Carlos Marques <psycarlo1@gmail.com>
2026-04-11 16:42:14 +0100

    chore: remove already in hermes pollyfills

--- apps/mobile/utils/polyfills.ts
+++ apps/mobile/utils/polyfills.ts
@@ -1,6 +1,7 @@
-// Polyfills for Array methods from the "change array by copy" proposal (ES2023).
-// Required because the Hermes build bundled with this React Native version does
-// not yet ship these methods.
+// Polyfill for Array.prototype.toSorted (ES2023).
+// Hermes does not yet ship this method natively.
+// Track: https://github.com/facebook/hermes/pull/1298
+// Remove this polyfill once that PR is merged and shipped with React Native.
 
 if (!Array.prototype.toSorted) {
   // eslint-disable-next-line no-extend-native
@@ -8,52 +9,6 @@ if (!Array.prototype.toSorted) {
     configurable: true,
     value<T>(this: T[], compareFn?: (a: T, b: T) => number): T[] {
       return [...this].sort(compareFn)
-    },
-    writable: true
-  })
-}
-
-if (!Array.prototype.toReversed) {
-  // eslint-disable-next-line no-extend-native
-  Object.defineProperty(Array.prototype, 'toReversed', {
-    configurable: true,
-    value<T>(this: T[]): T[] {
-      return [...this].reverse()
-    },
-    writable: true
-  })
-}
-
-if (!Array.prototype.toSpliced) {
-  // eslint-disable-next-line no-extend-native
-  Object.defineProperty(Array.prototype, 'toSpliced', {
-    configurable: true,
-    value<T>(
-      this: T[],
-      start: number,
-      deleteCount?: number,
-      ...items: T[]
-    ): T[] {
-      const copy = [...this]
-      if (deleteCount === undefined) {
-        copy.splice(start)
-      } else {
-        copy.splice(start, deleteCount, ...items)
-      }
-      return copy
-    },
-    writable: true
-  })
-}
-
-if (!Array.prototype.with) {
-  // eslint-disable-next-line no-extend-native
-  Object.defineProperty(Array.prototype, 'with', {
-    configurable: true,
-    value<T>(this: T[], index: number, value: T): T[] {
-      const copy = [...this]
-      copy[index < 0 ? copy.length + index : index] = value
-      return copy
     },
     writable: true
   })
