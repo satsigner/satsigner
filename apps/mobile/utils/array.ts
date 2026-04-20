93c890826afa4bc6e213349200121e0cac04451e
v4v2 <tai@tilde.team>
2026-04-20 09:08:07 -0300

    chore: format files

--- apps/mobile/utils/array.ts
+++ apps/mobile/utils/array.ts
@@ -1,18 +1,18 @@
 export function shuffle<T>(array: T[]): T[] {
-  const result = [...array];
+  const result = [...array]
 
-  for (let i = result.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
-    [result[i], result[j]] = [result[j], result[i]];
+  for (let i = result.length - 1; i > 0; i -= 1) {
+    const j = Math.floor(Math.random() * (i + 1))
+    ;[result[i], result[j]] = [result[j], result[i]]
   }
 
-  return result;
+  return result
 }
 
 export function range(end: number, start = 0) {
-  const result = [];
-  for (let i = start; i < end; i++) {
-    result.push(i);
+  const result = []
+  for (let i = start; i < end; i += 1) {
+    result.push(i)
   }
-  return result;
+  return result
 }
