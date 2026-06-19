3968f7f8d58dcdac470a37869937745a3ab29061
v4v2 <tai@tilde.team>
2026-06-18 23:42:06 -0300

    fix: refactor + add missing queries to remove child data upon account deletion

--- apps/mobile/db/mutations/accounts.ts
+++ apps/mobile/db/mutations/accounts.ts
@@ -1,12 +1,16 @@
+import { type NitroSQLiteConnection } from 'react-native-nitro-sqlite'
+
 import { type Account, type Key } from '@/types/models/Account'
 
 import { getDb, runTransaction } from '../connection'
 import { boolToInt, dateToIso, optionalToJson } from '../mappers'
 import { upsertAddresses } from './addresses'
 import { upsertLabels } from './labels'
-import { upsertNostrData } from './nostr'
+import { clearNostrData, upsertNostrData } from './nostr'
 import { upsertTransactions } from './transactions'
 import { upsertUtxos } from './utxos'
+
+type TransactionContext = NitroSQLiteConnection
 
 function keysToJson(keys: Key[]): string {
   return JSON.stringify(keys.map(({ secret: _s, iv: _iv, ...meta }) => meta))
@@ -65,9 +69,12 @@ function insertAccount(account: Account) {
   })
 }
 
-function updateAccountRow(account: Account) {
-  const db = getDb()
-  db.execute(
+function updateAccountRow(
+  account: Account,
+  connectionContext?: NitroSQLiteConnection
+) {
+  const dbConnection = connectionContext ?? getDb()
+  dbConnection.execute(
     `UPDATE accounts SET
       name = ?, network = ?, policy_type = ?, keys = ?,
       key_count = ?, keys_required = ?,
@@ -114,60 +121,20 @@ function updateAccountRow(account: Account) {
   )
 }
 
+function clearAccountChildData(tx: TransactionContext, accountId: string) {
+  tx.execute('DELETE FROM transactions WHERE account_id = ?', [accountId])
+  tx.execute('DELETE FROM utxos WHERE account_id = ?', [accountId])
+  tx.execute('DELETE FROM addresses WHERE account_id = ?', [accountId])
+  clearNostrData()
+}
+
 function updateFullAccount(account: Account) {
   runTransaction((tx) => {
     // Update account row
-    tx.execute(
-      `UPDATE accounts SET
-        name = ?, network = ?, policy_type = ?, keys = ?,
-        key_count = ?, keys_required = ?,
-        balance = ?, num_addresses = ?, num_transactions = ?,
-        num_utxos = ?, sats_in_mempool = ?,
-        last_synced_at = ?, sync_status = ?,
-        sync_progress_total = ?, sync_progress_done = ?,
-        nostr_auto_sync = ?, nostr_common_npub = ?, nostr_common_nsec = ?,
-        nostr_device_npub = ?, nostr_device_nsec = ?,
-        nostr_device_display_name = ?, nostr_device_picture = ?,
-        nostr_last_backup_fingerprint = ?, nostr_last_updated = ?, nostr_sync_start = ?,
-        nostr_npub_aliases = ?, nostr_npub_profiles = ?
-      WHERE id = ?`,
-      [
-        account.name,
-        account.network,
-        account.policyType,
-        keysToJson(account.keys),
-        account.keyCount,
-        account.keysRequired,
-        account.summary.balance,
-        account.summary.numberOfAddresses,
-        account.summary.numberOfTransactions,
-        account.summary.numberOfUtxos,
-        account.summary.satsInMempool,
-        dateToIso(account.lastSyncedAt),
-        account.syncStatus,
-        account.syncProgress?.totalTasks ?? null,
-        account.syncProgress?.tasksDone ?? null,
-        boolToInt(account.nostr?.autoSync),
-        account.nostr?.commonNpub ?? '',
-        account.nostr?.commonNsec ?? '',
-        account.nostr?.deviceNpub ?? null,
-        account.nostr?.deviceNsec ?? null,
-        account.nostr?.deviceDisplayName ?? null,
-        account.nostr?.devicePicture ?? null,
-        account.nostr?.lastBackupFingerprint ?? null,
-        dateToIso(account.nostr?.lastUpdated),
-        dateToIso(account.nostr?.syncStart),
-        optionalToJson(account.nostr?.npubAliases) ?? '{}',
-        optionalToJson(account.nostr?.npubProfiles) ?? '{}',
-        account.id
-      ]
-    )
+    updateAccountRow(account, tx)
 
     // Replace child data
-    tx.execute('DELETE FROM transactions WHERE account_id = ?', [account.id])
-    tx.execute('DELETE FROM utxos WHERE account_id = ?', [account.id])
-    tx.execute('DELETE FROM addresses WHERE account_id = ?', [account.id])
-
+    clearAccountChildData(tx, account.id)
     upsertTransactions(tx, account.id, account.transactions)
     upsertUtxos(tx, account.id, account.utxos)
     upsertAddresses(tx, account.id, account.addresses)
@@ -177,13 +144,21 @@ function updateFullAccount(account: Account) {
 }
 
 function deleteAccount(id: string) {
-  const db = getDb()
-  db.execute('DELETE FROM accounts WHERE id = ?', [id])
+  runTransaction((tx) => {
+    tx.execute('DELETE FROM accounts WHERE id = ?', [id])
+    clearAccountChildData(tx, id)
+  })
 }
 
 function deleteAllAccounts() {
-  const db = getDb()
-  db.execute('DELETE FROM accounts')
+  runTransaction((tx) => {
+    tx.execute('DELETE FROM accounts')
+
+    // Delete child data of all accounts
+    tx.execute('DELETE FROM transactions')
+    tx.execute('DELETE FROM utxos')
+    tx.execute('DELETE FROM addresses')
+  })
 }
 
 function updateAccountName(id: string, name: string) {
