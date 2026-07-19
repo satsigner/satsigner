bfb3891bd6b72d8be8c80f74dda816fd28b2691e
v4v2 <tai@tilde.team>
2026-07-19 16:28:36 -0300

    feat: add changelog section to settings > about (WIP)
    For now, the changelog is hardcoded and rendered plainly.

--- apps/mobile/app/(authenticated)/(tabs)/(signer,explorer,converter)/settings/about.tsx
+++ apps/mobile/app/(authenticated)/(tabs)/(signer,explorer,converter)/settings/about.tsx
@@ -1,5 +1,7 @@
 import { Stack } from 'expo-router'
+import { ScrollView, StyleSheet } from 'react-native'
 
+import CHANGELOG from '@/CHANGELOG.md'
 import SSText from '@/components/SSText'
 import { APP_VERSION, BUILD_NUMBER } from '@/constants/version'
 import SSHStack from '@/layouts/SSHStack'
@@ -19,13 +21,32 @@ export default function About() {
         }}
       />
       <SSMainLayout>
-        <SSVStack>
-          <SSHStack justifyBetween>
-            <SSText uppercase>{t('common.version')}</SSText>
-            <SSText>{`${APP_VERSION} (${BUILD_NUMBER})`}</SSText>
-          </SSHStack>
-        </SSVStack>
+        <ScrollView
+          showsVerticalScrollIndicator={false}
+          contentContainerStyle={styles.scroll}
+        >
+          <SSVStack gap="lg">
+            <SSHStack justifyBetween>
+              <SSText uppercase>{t('common.version')}</SSText>
+              <SSText>{`${APP_VERSION} (${BUILD_NUMBER})`}</SSText>
+            </SSHStack>
+            <SSVStack gap="sm">
+              <SSText uppercase weight="bold">
+                {t('settings.about.changeLog')}
+              </SSText>
+              <SSText type="mono" size="xs" color="muted">
+                {CHANGELOG}
+              </SSText>
+            </SSVStack>
+          </SSVStack>
+        </ScrollView>
       </SSMainLayout>
     </>
   )
 }
+
+const styles = StyleSheet.create({
+  scroll: {
+    paddingBottom: 64
+  }
+})
